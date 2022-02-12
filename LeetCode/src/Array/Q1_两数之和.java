87045d8945c36acacdcfd9b1c009877ea7892d5e
Hyper18 <windhyperspace@gmail.com>
2022-02-12 14:55:58 +0800

    1. 两数之和 re

--- LeetCode/src/Array/Q1_两数之和.java
+++ LeetCode/src/Array/Q1_两数之和.java
@@ -1,36 +1,40 @@
+package Array;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Vincent
  * 解题思路
- * 1.嵌套循环暴力求解，算法复杂度为O(n^2)
- * 2.使用Hashmap，算法复杂度为O(n)
+ * 1.嵌套循环暴力求解 -- 时间复杂度O(n^2)
+ * 2.使用Hashmap -- 时间复杂度O(n)
  */
 public class Q1_两数之和 {
     public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
         int num1, num2;
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
             num1 = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
+            for (int j = i + 1; j < len; j++) {
                 num2 = nums[j];
                 if (num1 + num2 == target) {
-                    result[0] = i;
-                    result[1] = j;
+                    res[0] = i;
+                    res[1] = j;
                 }
             }
         }
-        return result;
+        return res;
     }
 
     public int[] twoSum2(int[] nums, int target) {
-        int[] res = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int temp = target - nums[i];
-            if (map.containsKey(temp)) {
-                res[0] = map.get(temp);
+        int[] res = new int[2];
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            int tmp = target - nums[i];
+            if (map.containsKey(tmp)) {
+                res[0] = map.get(tmp);
                 res[1] = i;
             } else {
                 map.put(nums[i], i);
