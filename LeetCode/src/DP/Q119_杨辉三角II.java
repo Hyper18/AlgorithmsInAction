a8eaad182c27f1735edc89a6be55255da9f8114d
Hyper18 <windhyperspace@gmail.com>
2022-02-15 15:28:35 +0800

    119. 杨辉三角 II

---
+++ LeetCode/src/DP/Q119_杨辉三角II.java
@@ -0,0 +1,30 @@
+package DP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/15
+ * <p>
+ * 思路
+ * 递推
+ */
+public class Q119_杨辉三角II {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(row);
+        }
+
+        return res.get(rowIndex);
+    }
+}
