d7fb857cca735deb0c44bfac7c507c5fe30fd7f5
Hyper18 <windhyperspace@gmail.com>
2022-02-15 23:58:44 +0800

    73. 矩阵置零

---
+++ LeetCode/src/Matrix/Q73_矩阵置零.java
@@ -0,0 +1,83 @@
+package Matrix;
+
+import java.util.Arrays;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/15
+ * <p>
+ * 思路
+ * 1. 数组分别标记行、列，遍历两次 -- O(n*m)
+ * 2. 借助矩阵的首行和首列记录作为标志 -- O(n*m)
+ */
+public class Q73_矩阵置零 {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean row0 = false, col0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                Arrays.fill(matrix[i], 0);
+            }
+        }
+
+        if (row0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (col0) {
+            Arrays.fill(matrix[0], 0);
+        }
+    }
+}
