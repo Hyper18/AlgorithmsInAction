f165cef29f49b4f5312ab89aab7d99915847f389
Hyper18 <windhyperspace@gmail.com>
2022-02-15 14:25:25 +0800

    566. 重塑矩阵

---
+++ LeetCode/src/Matrix/Q566_重塑矩阵.java
@@ -0,0 +1,50 @@
+package Matrix;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/15
+ * <p>
+ * 思路
+ * 1. 模拟，不符合则输出原matrix -- O(rc)
+ * 2. 官方解法，优化了过渡数组，直接利用取余重塑 -- O(rc)
+ */
+public class Q566_重塑矩阵 {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+        int[] tmp = new int[m * n];
+        int idx = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tmp[idx++] = mat[i][j];
+            }
+        }
+
+        int[][] res = new int[r][c];
+        idx = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                res[i][j] = tmp[idx++];
+            }
+        }
+
+        return res;
+    }
+
+    public int[][] matrixReshape2(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+        for (int idx = 0; idx < m * n; ++idx) {
+            res[idx / c][idx % c] = mat[idx / n][idx % n];
+        }
+        return res;
+    }
+}
