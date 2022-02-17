75eb47ab87f642cd6094731cde8199a0c5187315
Hyper18 <windhyperspace@gmail.com>
2022-02-17 19:15:51 +0800

    基础练习 序列求和

---
+++ BlueBridgeCup/src/practice1/A_基础练习_序列求和.java
@@ -0,0 +1,20 @@
+package practice1;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class A_基础练习_序列求和 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+		long n = sc.nextLong();
+		long sum = n * (n + 1) / 2;
+		pw.println(sum);
+
+		sc.close();
+		pw.close();
+	}
+}
