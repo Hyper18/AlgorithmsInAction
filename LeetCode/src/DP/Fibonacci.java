70e1140ae65a987030af33bf7f30cbfd7dd741dd
Hyper18 <windhyperspace@gmail.com>
2022-02-17 12:37:07 +0800

    509. 斐波那契数

---
+++ LeetCode/src/DP/Fibonacci.java
@@ -0,0 +1,42 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/17
+ */
+public class Fibonacci {
+    public static void main(String[] args) throws Exception {
+        System.out.println(fibonacci(5));
+
+        for (int i = 0; i < 50; i++) {
+            dp[i] = -1;
+        }
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(fib(n));
+        bf.close();
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+
+    private static int[] dp = new int[50];
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return dp[0] = 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = fib(n - 2) + fib(n - 1);
+    }
+}
---
+++ LeetCode/src/DP/Q509_斐波那契数.java
@@ -0,0 +1,36 @@
+package DP;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/17
+ * <p>
+ * 思路
+ * 1. DP table
+ * 2. 状压DP
+ */
+public class Q509_斐波那契数 {
+    public int fib(int n) {
+        int[] dp = new int[35];
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int fib2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        int pre = 1;
+        int curr = 1;
+        for (int i = 3; i <= n; i++) {
+            int tmp = pre + curr;
+            pre = curr;
+            curr = tmp;
+        }
+        return curr;
+    }
+}
