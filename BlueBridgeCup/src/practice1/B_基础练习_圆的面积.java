c258e81e13d43a1c6ccb05c5da44d4526184cff7
Hyper18 <windhyperspace@gmail.com>
2022-02-17 19:29:02 +0800

    基础练习 圆的面积

---
+++ BlueBridgeCup/src/practice1/B_基础练习_圆的面积.java
@@ -0,0 +1,22 @@
+package practice1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class B_基础练习_圆的面积 {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer in = new StreamTokenizer(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+		in.nextToken();
+		int r = (int) in.nval;
+
+		double s = Math.PI * r * r;
+		out.printf("%.7f", s);
+
+		out.close();
+	}
+}
