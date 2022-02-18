25152c377d026997ea568c3507e782749ad055ec
Hyper18 <windhyperspace@gmail.com>
2022-02-18 19:55:21 +0800

    145. 二叉树的后序遍历

---
+++ LeetCode/src/Tree/Q145_二叉树的后序遍历.java
@@ -0,0 +1,52 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/18
+ */
+public class Q145_二叉树的后序遍历 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = root;
+        postorder(cur, res);
+
+        return res;
+    }
+
+    private void postorder(TreeNode cur, List<Integer> res) {
+        if (cur != null) {
+            postorder(cur.left, res);
+            postorder(cur.right, res);
+            res.add(cur.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while (cur != null || !stk.isEmpty()) {
+            if (cur != null) {
+                stk.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stk.peek();
+                if (cur.right != null && cur.right != pre) {
+                    cur = cur.right;
+                } else {
+                    cur = stk.pop();
+                    res.add(cur.val);
+                    pre = cur;
+                    cur = null;
+                }
+            }
+        }
+
+        return res;
+    }
+}
