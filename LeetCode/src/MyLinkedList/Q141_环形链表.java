c3bae0c8265eef4840934c92b21c53e1f7b18ef4
Hyper18 <windhyperspace@gmail.com>
2022-02-19 19:52:49 +0800

    141. 环形链表

---
+++ LeetCode/src/MyLinkedList/Q141_环形链表.java
@@ -0,0 +1,41 @@
+package MyLinkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/19
+ * <p>
+ * 1. set验重 -- O(n)
+ * 2. 快慢指针
+ */
+public class Q141_环形链表 {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (!set.add(cur)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
