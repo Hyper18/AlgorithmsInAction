2659a859f63d49d1de0fe87b4f69fec156c036f5
Hyper18 <windhyperspace@gmail.com>
2022-02-20 15:41:56 +0800

    83. 删除排序链表中的重复元素

---
+++ LeetCode/src/MyLinkedList/Q83_删除排序链表中的重复元素.java
@@ -0,0 +1,45 @@
+package MyLinkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/20
+ * <p>
+ * 1. set去重
+ * 2. 利用 已排序 特点
+ */
+public class Q83_删除排序链表中的重复元素 {
+    public ListNode deleteDuplicates(ListNode head) {
+        Set<Integer> seen = new HashSet<>();
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (seen.add(cur.next.val)) {
+                cur = cur.next;
+            } else {
+                cur.next = cur.next.next;
+            }
+        }
+
+        return head;
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val != cur.next.val) {
+                cur = cur.next;
+            } else {
+                cur.next = cur.next.next;
+            }
+        }
+
+        return head;
+    }
+}
