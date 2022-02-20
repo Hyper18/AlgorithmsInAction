bdbc50172e6665c3569a2e45ca638ed3b73348fc
Hyper18 <windhyperspace@gmail.com>
2022-02-20 17:02:51 +0800

    232. 用栈实现队列

---
+++ LeetCode/src/Stack_Queue/Q232_用栈实现队列.java
@@ -0,0 +1,45 @@
+package Stack_Queue;
+
+import java.util.Stack;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/20
+ */
+public class Q232_用栈实现队列 {
+    class MyQueue {
+        private Stack<Integer> in;
+        private Stack<Integer> out;
+
+        public MyQueue() {
+            in = new Stack();
+            out = new Stack();
+        }
+
+        public void push(int x) {
+            in.push(x);
+        }
+
+        public int pop() {
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+            return out.pop();
+        }
+
+        public int peek() {
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+            return out.peek();
+        }
+
+        public boolean empty() {
+            return in.isEmpty() && out.isEmpty();
+        }
+    }
+}
