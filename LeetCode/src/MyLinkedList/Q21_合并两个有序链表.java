6bb0d8662a656d85473bfd320cba17ae4a6e1835
Hyper18 <windhyperspace@gmail.com>
2022-02-20 17:25:24 +0800

    21. 合并两个有序链表 - 递归

--- LeetCode/src/MyLinkedList/Q21_合并两个有序链表.java
+++ LeetCode/src/MyLinkedList/Q21_合并两个有序链表.java
@@ -5,6 +5,7 @@ package MyLinkedList;
  * @date 2022/02/19
  * <p>
  * 1. 归并
+ * 2. 递归
  */
 public class Q21_合并两个有序链表 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
@@ -23,5 +24,19 @@ public class Q21_合并两个有序链表 {
         cur.next = list1 == null ? list2 : list1;
 
         return dummy.next;
+    }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists2(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists2(list1, list2.next);
+            return list2;
+        }
     }
 }
