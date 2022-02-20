81ad00f38836cf9cc4d91151b9a9bd9df62cd766
Hyper18 <windhyperspace@gmail.com>
2022-02-20 15:11:18 +0800

    203. 移除链表元素

---
+++ LeetCode/src/MyLinkedList/Q203_移除链表元素.java
@@ -0,0 +1,22 @@
+package MyLinkedList;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/20
+ */
+public class Q203_移除链表元素 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
