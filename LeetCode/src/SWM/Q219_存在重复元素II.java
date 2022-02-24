b33222d47d01e6ca5e83f4912fa6a99fb4f68fbf
Hyper18 <windhyperspace@gmail.com>
2022-02-24 16:12:49 +0800

    219. 存在重复元素 II

---
+++ LeetCode/src/SWM/Q219_存在重复元素II.java
@@ -0,0 +1,64 @@
+package SWM;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/24
+ * <p>
+ * 思路
+ * 1. 暴力 -- O(n^2)
+ * 2. hash记录下标 -- O(n)
+ * 2. SVM，set去重，随遍历不断移除窗口左侧的外部元素 -- O(n)
+ */
+public class Q219_存在重复元素II {
+    public static void main(String[] args) {
+        System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; Math.abs(i - j) <= k && j < len; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean containsNearbyDuplicate2(int[] nums, int k) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(i - map.get(nums[i])) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+
+    public static boolean containsNearbyDuplicate3(int[] nums, int k) {
+        int len = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < len; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
