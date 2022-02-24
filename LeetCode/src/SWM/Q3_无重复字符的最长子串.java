7e5c7cdb023ecfa86874efe16a36776033f80399
Hyper18 <windhyperspace@gmail.com>
2022-02-24 12:32:32 +0800

    3. 无重复字符的最长子串

---
+++ LeetCode/src/SWM/Q3_无重复字符的最长子串.java
@@ -0,0 +1,33 @@
+package SWM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/24
+ */
+public class Q3_无重复字符的最长子串 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int cnt = 0;
+        int left = 0;
+        char[] ss = s.toCharArray();
+        for (int i = 0; i < ss.length; i++) {
+            if (map.containsKey(ss[i])) {
+                left = Math.max(left, map.get(ss[i]) + 1);
+            }
+            map.put(ss[i], i);
+            cnt = Math.max(cnt, i - left + 1);
+        }
+
+        return cnt;
+    }
+}
