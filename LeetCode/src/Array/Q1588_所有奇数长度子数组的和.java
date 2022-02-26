495b1b46a43ae93ea6629cfd13d073fee775a4bf
Hyper18 <windhyperspace@gmail.com>
2022-02-26 16:59:45 +0800

    1588. 所有奇数长度子数组的和

--- LeetCode/src/Array/Q1588_所有奇数长度子数组的和.java
+++ LeetCode/src/Array/Q1588_所有奇数长度子数组的和.java
@@ -7,6 +7,9 @@ package Array;
  * 思路
  * 1. 暴力 -- O(n^3)
  * 2. 前缀和 -- O(n^2)
+ * 以空间换时间，先通过相同长度的数组依次存储前缀和
+ * 将计算每个子数组的和的时间复杂度从 O(n) 降低到 O(1)
+ * 再遍历获取每个奇数长子数组的和的值
  */
 public class Q1588_所有奇数长度子数组的和 {
     public int sumOddLengthSubarrays(int[] arr) {
