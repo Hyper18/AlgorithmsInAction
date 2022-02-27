2680169fa1a301bf9879dd470b4ee92b6f400da8
Hyper18 <windhyperspace@gmail.com>
2022-02-27 11:14:21 +0800

    560. 和为 K 的子数组

---
+++ LeetCode/src/Array/Q560_和为K的子数组.java
@@ -0,0 +1,48 @@
+package Array;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/27
+ * <p>
+ * 思路
+ * 1. 模拟 -- O(n^2)
+ * 2. 前缀和 -- O(n^2)
+ */
+public class Q560_和为K的子数组 {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0;
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int end = start; end >= 0; end--) {
+                sum += nums[end];
+                if (sum == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int n = nums.length;
+        int[] preSums = new int[n + 1];
+        preSums[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            preSums[i + 1] = preSums[i] + nums[i];
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (preSums[j + 1] - preSums[i] == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
