1d6cd71724e19dee18caa19c7af3bbbbcc24b1a3
Hyper18 <windhyperspace@gmail.com>
2022-02-28 22:22:56 +0800

    算法训练 调和数列问题

---
+++ BlueBridgeCup/src/practice3/C_调和数列问题.java
@@ -0,0 +1,37 @@
+package practice3;
+
+import java.io.BufferedInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2022/02/28
+ */
+public class C_调和数列问题 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        double x;
+        while ((x = in.nextDouble()) != 0) {
+            out.println(cal(x));
+        }
+
+        in.close();
+        out.close();
+    }
+
+    private static String cal(double x) {
+        double sum = 0;
+        int n = 1;
+        sum += 1.0 / (n + 1);
+        while (sum < x) {
+            n++;
+            sum += 1.0 / (n + 1);
+        }
+
+        return n + " card(s)";
+    }
+}
