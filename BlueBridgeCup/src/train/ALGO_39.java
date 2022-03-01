753683577d13383a2fc736832d29127d4106dd81
Hyper18 <windhyperspace@gmail.com>
2022-03-01 22:08:51 +0800

    ALGO-39	VIP试题 数组排序去重

---
+++ BlueBridgeCup/src/train/ALGO_39.java
@@ -0,0 +1,51 @@
+package train;
+
+import java.io.BufferedInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/01
+ * <p>
+ * 思路
+ * set去重+sort
+ * 注：注意hashset的默认排列
+ */
+public class ALGO_39 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = in.nextInt();
+        }
+        Arrays.sort(nums);
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+            }
+        }
+
+        int[] res = new int[set.size()];
+        int k = 0;
+        for (int num : set) {
+            res[k++] = num;
+        }
+
+        Arrays.sort(res);
+        for (int i = 0; i < res.length; i++) {
+            out.println(res[i]);
+        }
+
+        in.close();
+        out.close();
+    }
+}
