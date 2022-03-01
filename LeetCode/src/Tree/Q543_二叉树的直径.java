55516649b5a8fa316d7358ff0fa93443c8d87caa
Hyper18 <windhyperspace@gmail.com>
2022-03-01 12:51:47 +0800

    543. 二叉树的直径

---
+++ LeetCode/src/Tree/Q543_二叉树的直径.java
@@ -0,0 +1,27 @@
+package Tree;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/01
+ */
+public class Q543_二叉树的直径 {
+    int ans = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        TreeNode cur = root;
+        DFSTraverse(cur);
+
+        return ans;
+    }
+
+    private int DFSTraverse(TreeNode cur) {
+        if (cur == null) {
+            return 0;
+        }
+        int left = DFSTraverse(cur.left);
+        int right = DFSTraverse(cur.right);
+        ans = Math.max(ans, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
