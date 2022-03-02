98fbcfade743f3e44cdb507d4b690f0f63cf7e1f
Hyper18 <windhyperspace@gmail.com>
2022-03-02 14:21:22 +0800

    ALGO-80	VIP试题 整数平均值

---
+++ BlueBridgeCup/src/train/ALGO_80.java
@@ -0,0 +1,30 @@
+package train;
+
+import java.io.BufferedInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/02
+ */
+public class ALGO_80 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedInputStream(System.in));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+		int n = in.nextInt();
+		int[] nums = new int[n];
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			nums[i] = in.nextInt();
+			sum += nums[i];
+		}
+
+		out.println(sum / n);
+
+		in.close();
+		out.close();
+	}
+}
