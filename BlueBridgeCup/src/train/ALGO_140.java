a97f0c8c37f8b5c7dab95eb3481da5208b1e6cc6
Hyper18 <windhyperspace@gmail.com>
2022-03-02 19:01:47 +0800

    ALGO-140 VIP试题 P1101

---
+++ BlueBridgeCup/src/train/ALGO_140.java
@@ -0,0 +1,74 @@
+package train;
+
+import java.io.BufferedInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/02
+ * <p>
+ * 思路
+ * 使用静态类表示prut结构体
+ */
+public class ALGO_140 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int n = in.nextInt();
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            prut p = new prut(in.next(), in.nextDouble(), in.nextInt());
+            sum += p.getDj() * p.getSl();
+        }
+
+        out.printf("%.6f", sum);
+
+        in.close();
+        out.close();
+    }
+
+    private static class prut {
+        String mc;
+        double dj;
+        int sl;
+
+        public prut() {
+            super();
+        }
+
+        public prut(String mc, double dj, int sl) {
+            super();
+            this.mc = mc;
+            this.dj = dj;
+            this.sl = sl;
+        }
+
+        public String getMc() {
+            return mc;
+        }
+
+        public void setMc(String mc) {
+            this.mc = mc;
+        }
+
+        public double getDj() {
+            return dj;
+        }
+
+        public void setDj(double dj) {
+            this.dj = dj;
+        }
+
+        public int getSl() {
+            return sl;
+        }
+
+        public void setSl(int sl) {
+            this.sl = sl;
+        }
+    }
+}
