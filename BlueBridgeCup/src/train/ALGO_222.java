03161a9ad0a985ec6331dd06073afbbf16c85f6b
Hyper18 <windhyperspace@gmail.com>
2022-03-02 21:06:38 +0800

    ALGO-222	VIP试题 数组逆序排列

---
+++ BlueBridgeCup/src/train/ALGO_222.java
@@ -0,0 +1,49 @@
+package train;
+
+import java.io.BufferedInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/02
+ * <p>
+ * 双指针
+ * 这个题设计swap不是给java用的。。
+ */
+public class ALGO_222 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int[] nums = new int[20];
+        int idx = 0;
+        while (true) {
+            int num = in.nextInt();
+            if (num == 0) {
+                break;
+            }
+            nums[idx++] = num;
+        }
+
+        int start = 0;
+        int end = idx - 1;
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = tmp;
+        }
+
+        for (int i = 0; i < idx; i++) {
+            if (i == idx - 1) {
+                out.print(nums[i]);
+                break;
+            }
+            out.print(nums[i] + " ");
+        }
+
+        in.close();
+        out.close();
+    }
+}
