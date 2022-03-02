71deea9f511ed7474bf562a075df30e4949f99b3
Hyper18 <windhyperspace@gmail.com>
2022-03-02 11:06:04 +0800

    ALGO-67	VIP试题 最大值与最小值的计算

--- BlueBridgeCup/src/train/ALGO_67.java
+++ BlueBridgeCup/src/train/ALGO_67.java
@@ -9,24 +9,55 @@ import java.util.Scanner;
 /**
  * @author Hyperspace
  * @date 2022/03/01
+ * <p>
+ * 思路
+ * 1. 流
+ * 2. 遍历
  */
 public class ALGO_67 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(new BufferedInputStream(System.in));
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 
-		int[] nums = new int[11];
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] = in.nextInt();
-		}
+        int[] nums = new int[11];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = in.nextInt();
+        }
 
-		int min = Arrays.stream(nums).min().getAsInt();
-		int max = Arrays.stream(nums).max().getAsInt();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
 
-		out.print(max + " " + min);
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
 
-		in.close();
-		out.close();
-	}
+        out.print(max + " " + min);
+
+        in.close();
+        out.close();
+    }
+
+    public static void main2(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int[] nums = new int[11];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+
+        out.print(max + " " + min);
+
+        in.close();
+        out.close();
+    }
 
 }
