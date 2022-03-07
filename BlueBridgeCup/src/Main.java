caf96b3b1ae568cd42604ab5808651ed798d8b9a
Hyper18 <windhyperspace@gmail.com>
2022-03-07 08:45:39 +0800

    Model

--- BlueBridgeCup/src/Main.java
+++ BlueBridgeCup/src/Main.java
@@ -1,35 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 /**
  * 提交代码不要加入import 包名！！
  * JDK8下引用包仍需正常导入
  *
- * @Author Hyperspace
- * @Date 2022/02/09
+ * @author Hyperspace
+ * @date 2022/02/09
  * <p>
  * 主类必须用Main！！
  */
 public class Main {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer token = new StringTokenizer("");
-    public static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         int n = nextInt();
         long m = nextLong();
         double d = nextDouble();
         String s = nextString();
 
-        out.printf("%d %f %f %s", n, m, d, s);
+        out.printf("%d %d %f %s\n", n, m, d, s);
 
         close();
     }
 
-    public static int nextInt() throws Exception {
+    public static int nextInt() throws IOException {
         while (!token.hasMoreTokens()) {
             token = new StringTokenizer(in.readLine());
         }
@@ -37,7 +34,7 @@ public class Main {
         return Integer.parseInt(token.nextToken());
     }
 
-    public static long nextLong() throws Exception {
+    public static long nextLong() throws IOException {
         while (!token.hasMoreTokens()) {
             token = new StringTokenizer(in.readLine());
         }
@@ -45,7 +42,7 @@ public class Main {
         return Long.parseLong(token.nextToken());
     }
 
-    public static double nextDouble() throws Exception {
+    public static double nextDouble() throws IOException {
         while (!token.hasMoreTokens()) {
             token = new StringTokenizer(in.readLine());
         }
@@ -53,7 +50,7 @@ public class Main {
         return Double.parseDouble(token.nextToken());
     }
 
-    public static String nextString() throws Exception {
+    public static String nextString() throws IOException {
         while (!token.hasMoreTokens()) {
             token = new StringTokenizer(in.readLine());
         }
@@ -61,7 +58,7 @@ public class Main {
         return token.nextToken();
     }
 
-    public static void close() throws Exception {
+    public static void close() throws IOException {
         in.close();
         out.close();
     }
