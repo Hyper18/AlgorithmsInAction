8c992b888105e4c0288e5b598c12fc46b824e768
Hyper18 <windhyperspace@gmail.com>
2022-03-08 17:34:28 +0800

    45. 跳跃游戏 II

---
+++ LeetCode/src/DP/Q45_跳跃游戏II.java
@@ -0,0 +1,23 @@
+package DP;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/08
+ * <p>
+ * 贪心 + dp + 双指针 -- O(n)
+ * 待回看
+ */
+public class Q45_跳跃游戏II {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 1, j = 0; i < n; i++) {
+            while (j + nums[j] < i) {
+                j++;
+            }
+            dp[i] = dp[j] + 1;
+        }
+
+        return dp[n - 1];
+    }
+}
