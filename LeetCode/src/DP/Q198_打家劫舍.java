37806e2629e5c2a366ae213b6c5439529df75467
Hyper18 <windhyperspace@gmail.com>
2022-03-08 15:09:15 +0800

    198. 打家劫舍

---
+++ LeetCode/src/DP/Q198_打家劫舍.java
@@ -0,0 +1,49 @@
+package DP;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/08
+ * <p>
+ * 主要思考点在于找到状态转移方程
+ * 另注意dp[1]的选取
+ * 1. dp -- O(n)
+ * 2. 状压dp -- O(n)
+ */
+public class Q198_打家劫舍 {
+    public static void main(String[] args) {
+        System.out.println(rob2(new int[]{2, 1}));
+    }
+
+    public static int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = nums[1] > dp[0] ? nums[1] : dp[0];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] > dp[i - 2] + nums[i] ? dp[i - 1] : dp[i - 2] + nums[i];
+        }
+
+        return dp[n - 1];
+    }
+
+    public static int rob2(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int n = nums.length;
+        int pre = nums[0];
+        int curr = nums[1] > pre ? nums[1] : pre;
+
+        for (int i = 2; i < n; i++) {
+            int tmp = curr > pre + nums[i] ? curr : pre + nums[i];
+            pre = curr;
+            curr = tmp;
+        }
+
+        return curr;
+    }
+}
