cc488217cfa38c27ae7678dbf231186f115d61a0
Hyper18 <windhyperspace@gmail.com>
2022-03-20 21:03:39 +0800

    波动数列【第五届】【省赛】【A组】

--- BlueBridgeCup/src/Y2014A/PREV_318.java
+++ BlueBridgeCup/src/Y2014A/PREV_318.java
@@ -6,33 +6,62 @@ import java.util.StringTokenizer;
 /**
  * @author Hyperspace
  * @date 2022/03/17
+ * @time 312ms
+ * 转化为01背包问题
+ * s：背包最大容量
  */
 public class PREV_318 {
-	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	public static StringTokenizer token = new StringTokenizer("");
-	public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-	private static final int MOD = 100000000 + 7;
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer token = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    private static final int MOD = 100000000 + 7;
+    private static final int MAXN = 1000000 + 10;
+    private static int[] dp = new int[MAXN];
+    static int n;
 
-	public static void main(String[] args) throws IOException {
-		int n = nextInt();
-		int s = nextInt();
-		int a = nextInt();
-		int b = nextInt();
+    public static void main(String[] args) throws IOException {
+        n = nextInt();
+        int s = nextInt();
+        int a = nextInt();
+        int b = nextInt();
 
+        int num = (n - 1) * n / 2;
+        s += num * b;
+        // key step：后续如果直接强转会出现截断
+        long optNum = a + b;
+        init();
+        int ans = 0;
+        for (int i = 0; i <= num; i++) {
+            long tmp = s - i * optNum;
+            if (tmp % n == 0) {
+                ans = (ans + dp[i]) % MOD;
+            }
+        }
 
-		close();
-	}
+        out.println(ans);
 
-	public static int nextInt() throws IOException {
-		while (!token.hasMoreTokens()) {
-			token = new StringTokenizer(in.readLine());
-		}
+        close();
+    }
 
-		return Integer.parseInt(token.nextToken());
-	}
+    private static void init() {
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = (i + 1) * i / 2; j >= i; j--) {
+                dp[j] = (dp[j] + dp[j - i]) % MOD;
+            }
+        }
+    }
 
-	public static void close() throws IOException {
-		in.close();
-		out.close();
-	}
+    public static int nextInt() throws IOException {
+        while (!token.hasMoreTokens()) {
+            token = new StringTokenizer(in.readLine());
+        }
+
+        return Integer.parseInt(token.nextToken());
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
 }
