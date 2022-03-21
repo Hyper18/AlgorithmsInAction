09ae7df87abbe2ad55c7f52d18980e35c0344b65
Hyper18 <windhyperspace@gmail.com>
2022-03-21 20:48:02 +0800

    2014年 Java C组

---
+++ BlueBridgeCup/src/Y2014C/Q2等额本金.java
@@ -0,0 +1,11 @@
+package Y2014C;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/21
+ */
+public class Q2等额本金 {
+    public static void main(String[] args) {
+        System.out.printf("%.2f", 1250 + ((30000 - (1250 * (15 - 1))) * 0.005));
+    }
+}
