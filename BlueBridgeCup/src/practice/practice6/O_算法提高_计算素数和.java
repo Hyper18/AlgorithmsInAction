7f94ed249c6b74c68e12d910e629b0bb53b24f98
Hyper18 <windhyperspace@gmail.com>
2022-03-22 20:50:10 +0800

    算法提高 计算素数和

---
+++ BlueBridgeCup/src/practice/practice6/O_算法提高_计算素数和.java
@@ -0,0 +1,87 @@
+package practice.practice6;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/22
+ * @time 62ms
+ * 埃氏筛
+ * 感觉在原有方法上，掌握这种方法即可
+ */
+public class O_算法提高_计算素数和 {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer tk = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int ans = countPrime(n);
+        out.println(ans);
+        close();
+    }
+
+    private static int countPrime(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                ans += i;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int nextInt() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+
+        return Integer.parseInt(tk.nextToken());
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    public static String nextString() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+
+        return tk.nextToken();
+    }
+
+    public static long nextLong() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+
+        return Long.parseLong(tk.nextToken());
+    }
+
+    public static double nextDouble() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+
+        return Double.parseDouble(tk.nextToken());
+    }
+}
