642ed29194f05503b92d338047e0e06876037443
Hyper18 <windhyperspace@gmail.com>
2022-03-27 11:26:17 +0800

    12. 整数转罗马数字

---
+++ LeetCode/src/Greedy/Q12_整数转罗马数字.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/27
+ * 贪心法
+ * 从大到小枚举可能出现的最大数值，
+ * 拼接字符串输出
+ */
+public class Q12_整数转罗马数字 {
+    int[] nums = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    String[] strs = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 12; i >= 0; i--) {
+            while (num >= nums[i]) {
+                num -= nums[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
