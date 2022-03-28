3ae8cdbc18bc87ab1a4e85fd9aaad28cc5150e35
Hyper18 <windhyperspace@gmail.com>
2022-03-28 17:18:26 +0800

    279. 完全平方数

---
+++ LeetCode/src/DP/Q279_完全平方数.java
@@ -0,0 +1,35 @@
+package DP;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/28
+ * dp
+ * 1. 完全背包
+ * 2. 优化的dp
+ */
+public class Q279_完全平方数 {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static int numSquares2(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+            dp[i] = min + 1;
+        }
+
+        return dp[n];
+    }
+}
