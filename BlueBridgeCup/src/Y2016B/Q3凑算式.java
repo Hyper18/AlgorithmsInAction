23b02a319d1391ea1db9860613b7a75d98b6dad0
Hyper18 <windhyperspace@gmail.com>
2022-03-28 22:29:37 +0800

    2016年 Java B组

---
+++ BlueBridgeCup/src/Y2016B/Q3凑算式.java
@@ -0,0 +1,50 @@
+package Y2016B;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/26
+ * 注意分式的除法
+ * 直接除会产生截断
+ * 应先通分
+ */
+public class Q3凑算式 {
+    private static int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static int n = nums.length, ans;
+
+    public static void main(String[] args) {
+        f(0);
+        System.out.println(ans);
+    }
+
+    private static void f(int k) {
+        if (k == n) {
+            if (check()) {
+                ans++;
+            }
+        }
+        for (int i = k; i < n; i++) {
+            swap(i, k);
+            f(k + 1);
+            swap(i, k);
+        }
+    }
+
+    private static boolean check() {
+        int a = nums[0];
+        int b1 = nums[1];
+        int b2 = nums[2];
+        int c1 = nums[3] * 100 + nums[4] * 10 + nums[5];
+        int c2 = nums[6] * 100 + nums[7] * 10 + nums[8];
+        // key step
+        if ((b1 * c2 + c1 * b2) % (b2 * c2) == 0 && (a + (b1 * c2 + c1 * b2) / (b2 * c2) == 10)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void swap(int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
---
+++ BlueBridgeCup/src/Y2016B/Q6方格填数.java
@@ -0,0 +1,50 @@
+package Y2016B;
+
+/**
+ * @author Hyperspace
+ * @date 2022/03/28
+ * 采用朴素的检查方法，直接枚举出所有可能的操作
+ * 本质上还是在做全排列
+ */
+public class Q6方格填数 {
+	private static int[] nums = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	static int n = nums.length, ans;
+	private static boolean[] vis = new boolean[n];
+	private static int[] arr = new int[n];
+
+	public static void main(String[] args) {
+		f(0);
+		System.out.println(ans);
+	}
+
+	private static void f(int k) {
+		if (k == n) {
+			if (check()) {
+				ans++;
+			}
+			return;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (!vis[i]) {
+				vis[i] = true;
+				arr[k] = i;
+				f(k + 1);
+				vis[i] = false;
+			}
+		}
+	}
+
+	private static boolean check() {
+		if (Math.abs(arr[0] - arr[1]) == 1 || Math.abs(arr[0] - arr[3]) == 1 || Math.abs(arr[0] - arr[4]) == 1
+				|| Math.abs(arr[0] - arr[5]) == 1 || Math.abs(arr[1] - arr[2]) == 1 || Math.abs(arr[1] - arr[4]) == 1
+				|| Math.abs(arr[1] - arr[5]) == 1 || Math.abs(arr[1] - arr[6]) == 1 || Math.abs(arr[2] - arr[6]) == 1
+				|| Math.abs(arr[2] - arr[5]) == 1 || Math.abs(arr[3] - arr[4]) == 1 || Math.abs(arr[3] - arr[7]) == 1
+				|| Math.abs(arr[3] - arr[8]) == 1 || Math.abs(arr[4] - arr[5]) == 1 || Math.abs(arr[4] - arr[7]) == 1
+				|| Math.abs(arr[4] - arr[8]) == 1 || Math.abs(arr[4] - arr[9]) == 1 || Math.abs(arr[5] - arr[6]) == 1
+				|| Math.abs(arr[5] - arr[8]) == 1 || Math.abs(arr[5] - arr[9]) == 1 || Math.abs(arr[7] - arr[8]) == 1
+				|| Math.abs(arr[8] - arr[9]) == 1 || Math.abs(arr[6] - arr[9]) == 1) {
+			return false;
+		}
+		return true;
+	}
+}
