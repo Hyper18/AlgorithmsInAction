c367a76e92b3db7ea18f49c77cb579891c36fb9d
Hyper18 <windhyperspace@gmail.com>
2022-04-20 11:44:21 +0800

    20th Apr

---
+++ LeetCode/src/DFS/M388_文件的最长绝对路径.java
@@ -0,0 +1,45 @@
+package DFS;
+
+import java.util.Arrays;
+
+/**
+ * @author Hyperspace
+ * @date 2022/04/20
+ */
+public class M388_文件的最长绝对路径 {
+    static final int MAX = 10000 + 10;
+    static int[] dirs = new int[MAX];
+
+    public int lengthLongestPath(String input) {
+        Arrays.fill(dirs, -1);
+        int n = input.length();
+        int ans = 0;
+
+        int i = 0;
+        while (i < n) {
+            int level = 0;
+            while (i < n && input.charAt(i) == '\t') {
+                level++;
+                i++;
+            }
+            int j = i;
+            boolean isDir = true;
+            while (j < n && input.charAt(j) != '\n') {
+                if (input.charAt(j++) == '.') {
+                    isDir = false;
+                }
+            }
+            int cur = j - i;
+            int prev = level - 1 >= 0 ? dirs[level - 1] : -1;
+            int path = prev + 1 + cur;
+            if (isDir) {
+                dirs[level] = path;
+            } else if (path > ans) {
+                ans = path;
+            }
+            i = j + 1;
+        }
+
+        return ans;
+    }
+}
