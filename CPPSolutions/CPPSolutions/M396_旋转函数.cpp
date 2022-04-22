302565fcbdbbdb131981c5496f0327882e0c5e4e
Hyper18 <windhyperspace@gmail.com>
2022-04-22 10:26:46 +0800

    396. 旋转函数

---
+++ CPPSolutions/CPPSolutions/M396_旋转函数.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += i * nums[i];
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        for (int i = n - 1, cur = ans; i >= 0; i--) {
+            cur += sum - n * nums[i];
+            ans = max(ans, cur);
+        }
+
+        return ans;
+    }
+};
