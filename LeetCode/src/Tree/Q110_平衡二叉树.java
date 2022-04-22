2690cd3cb30b3d61c303acd6164e382753cb4e17
Hyper18 <windhyperspace@gmail.com>
2022-04-22 20:12:23 +0800

    110. 平衡二叉树

---
+++ LeetCode/src/Tree/Q110_平衡二叉树.java
@@ -0,0 +1,23 @@
+package Tree;
+
+/**
+ * @author Hyperspace
+ * @date 2022/04/22
+ * 递归
+ * 从根节点开始
+ * 按 左子树 -> 右子树顺序依次判断子树高度
+ * 若左子树与右子树高度差大于1，则不符合条件
+ */
+public class Q110_平衡二叉树 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(f(root.left) - f(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    private int f(TreeNode node) {
+        return node == null ? 0 : Math.max(f(node.left), f(node.right)) + 1;
+    }
+}
