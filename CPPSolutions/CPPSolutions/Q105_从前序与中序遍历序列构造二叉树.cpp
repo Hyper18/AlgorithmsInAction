173c3f011ce9997c8d716b318e0b9791d7036421
Hyper18 <windhyperspace@gmail.com>
2022-04-23 11:15:47 +0800

    105. 从前序与中序遍历序列构造二叉树 优化

--- CPPSolutions/CPPSolutions/Q105_从前序与中序遍历序列构造二叉树.cpp
+++ CPPSolutions/CPPSolutions/Q105_从前序与中序遍历序列构造二叉树.cpp
@@ -1,10 +1,17 @@
 using namespace std;
 #include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
+    unordered_map<int, int> idx_map; // 记录中序遍历的val-idx对应关系
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = preorder.size();
+        int idx = 0;
+        for (auto& val : inorder) {
+            idx_map[val] = idx++;
+        }
+        int n = inorder.size();
         return build(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 
@@ -12,19 +19,13 @@ public:
         if (preLeft > preRight) {
             return NULL;
         }
-        int preRootPos = preLeft;
-        int preRootVal = preorder[preRootPos];
-        TreeNode* root = new TreeNode(preRootVal);
-        int inRootPos = inLeft;
-        for (int i = inLeft; i <= inRight; i++) {
-            if (inorder[i] == preRootVal) {
-                inRootPos = i;
-                break;
-            }
-        }
-        int leftSubTrees = inRootPos - inLeft;
-        root->left = build(preorder, inorder, preLeft + 1, preLeft + leftSubTrees, inLeft, inRootPos - 1);
-        root->right = build(preorder, inorder, preLeft + leftSubTrees, preRight, inRootPos + 1, inRight);
+        int rootVal = preorder[preLeft];
+        TreeNode* root = new TreeNode(rootVal);
+        int rootPos = idx_map.at(rootVal);
+
+        int leftSubTrees = rootPos - inLeft;
+        root->left = build(preorder, inorder, preLeft + 1, preLeft + leftSubTrees, inLeft, rootPos - 1);
+        root->right = build(preorder, inorder, preLeft + leftSubTrees + 1, preRight, rootPos + 1, inRight);
 
         return root;
     }
