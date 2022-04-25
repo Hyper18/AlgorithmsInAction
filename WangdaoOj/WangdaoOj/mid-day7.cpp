b5bc623b4a9119df35582ff007abb4e04a8cd25c
Hyper18 <windhyperspace@gmail.com>
2022-04-25 20:48:41 +0800

    数据结构 - 二叉树的实现

--- WangdaoOj/WangdaoOj/mid-day7.cpp
+++ WangdaoOj/WangdaoOj/mid-day7.cpp
@@ -3,17 +3,52 @@ using namespace std;
 #include <cstdlib>
 #include <iostream>
 
-int main() {
-    char nums[10];
-    cin >> nums;
-}
+#define OK 0
+#define ERROR -1
+#define OVERFLOW -2
+
+typedef int Status;
 
 // Definition for a binary tree node.
-struct TreeNode {
-    int val;
+typedef struct TreeNode {
+    char val;
     TreeNode* left;
     TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
+}TreeNode, *BinaryTree;
+
+Status InitTree(BinaryTree& T) {
+    T = NULL;
+    
+    return OK;
+}
+
+void Build(char* nums, int sub, int len, TreeNode*& tree) {
+    if (sub >= len)
+        return;
+    tree = new TreeNode;
+    tree->val = nums[sub];
+    Build(nums, sub * 2 + 1, len, tree->left);
+    Build(nums, sub * 2 + 2, len, tree->right);
+}
+
+void Print(TreeNode* p) {
+    if (p == NULL) {
+        return;
+    }
+    cout << p->val;
+    Print(p->left);
+    Print(p->right);
+}
+
+int main() {
+    char nums[10];
+    cin >> nums;
+    BinaryTree tree;
+
+    InitTree(tree);
+    Build(nums, 0, 10, tree);
+    Print(tree);
+}
