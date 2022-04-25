166273b03ec79cca0a6deb511f17a780cb1ec37e
Hyper18 <windhyperspace@gmail.com>
2022-04-25 15:17:34 +0800

    868. 二进制间距

--- CPPSolutions/CPPSolutions/Q868_二进制间距.cpp
+++ CPPSolutions/CPPSolutions/Q868_二进制间距.cpp
@@ -1,9 +1,20 @@
 using namespace std;
-#include <cstdio>
+#include <algorithm>
 
 class Solution {
 public:
     int binaryGap(int n) {
+        int ans = 0, last = -1;
+        for (int i = 0; n > 0; i++) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    ans = max(ans, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+        }
 
+        return ans;
     }
 };
