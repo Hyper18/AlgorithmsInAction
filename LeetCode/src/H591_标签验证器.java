728f141b16d686d85d07dc76a6b5cf08f3778820
Hyper18 <windhyperspace@gmail.com>
2022-05-02 08:38:25 +0800

    2nd, May - raw

---
+++ LeetCode/src/H591_标签验证器.java
@@ -0,0 +1,15 @@
+/**
+ * @author Hyperspace
+ * @date 2022/05/02
+ */
+public class H591_标签验证器 {
+    public boolean isValid(String code) {
+        code = code.replaceAll("<!\\[CDATA\\[.*?\\]\\]>|t", "-");
+        String temp = "";
+        while (!temp.equals(code)) {
+            temp = code;
+            code = code.replaceFirst("<([A-Z]{1,9})>[^<]*</\\1>", "t");
+        }
+        return "t".equals(code);
+    }
+}
