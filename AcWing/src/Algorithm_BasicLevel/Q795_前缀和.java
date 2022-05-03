b072bb91ae1035f70b7a437f51c28a735c1588c3
Hyper18 <windhyperspace@gmail.com>
2022-05-03 16:41:03 +0800

    795. 前缀和

---
+++ AcWing/src/Algorithm_BasicLevel/Q795_前缀和.java
@@ -0,0 +1,45 @@
+package Algorithm_BasicLevel;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * @author Hyperspace
+ * @date 2022/05/03
+ */
+public class Q795_前缀和 {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer tk = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    private static final int N = 100000 + 10;
+    private static int[] pre = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt(), m = nextInt();
+        // 初始化前缀和
+        for (int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + nextInt();
+        }
+
+        while (m-- > 0) {
+            int l = nextInt(), r = nextInt();
+            // 计算区间和
+            out.println(pre[r] - pre[l - 1]);
+        }
+
+        close();
+    }
+
+    public static int nextInt() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+
+        return Integer.parseInt(tk.nextToken());
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
+}
---
+++ CPPSolutions/CPPSolutions/Q795_前缀和.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+
+const int N = 10e5 + 10;
+int pre[N];
+int n, m, k, l, r;
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &k);
+		pre[i] = pre[i - 1] + k;
+	}
+
+	while (m--) {
+		scanf("%d %d", &l, &r);
+		printf("%d\n", pre[r] - pre[l - 1]);
+	}
+}
