2ec2cb4346c5bf32bb10d030122e1d4654e90e53
Hyper18 <windhyperspace@gmail.com>
2022-05-07 22:51:17 +0800

    842. 排列数字

---
+++ CPPSolutions/CPPSolutions/Q842_排列数字.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include <iostream>
+
+const int N = 10;
+
+int path[N];
+int n;
+
+void dfs(int k, int st) {
+	if (k == n) {
+		for (int i = 0; i < n; i++) {
+			printf("%d ", path[i]);
+		}
+		puts("");
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!(st >> i & 1)) {
+			path[k] = i + 1;
+			dfs(k + 1, st + (1 << i));
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+	dfs(0, 0);
+}
