a6e1daed5a08628ef49243477169178a45274c92
Hyper18 <windhyperspace@gmail.com>
2022-05-09 20:33:38 +0800

    1012. 友好城市

---
+++ AcWing/src/DP/LISModel/Q1012_友好城市.java
@@ -0,0 +1,70 @@
+package DP.LISModel;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * @author Hyperspace
+ * @date 2022/05/09
+ * <p>
+ * 思路 线性dp
+ * 1. 状态表示
+ * 集合：f[i]表示从最左端城市到第i个城市所有可行申请的最大数目
+ * 属性：Max
+ * 2. 状态计算
+ * f[i] = max(f[i], f[j] + 1)
+ */
+public class Q1012_友好城市 {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer tk = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    private static final int N = 5000 + 10;
+    private static PII[] a = new PII[N];
+    private static int[] f = new int[N];
+
+    public static class PII {
+        int x, y;
+
+        public PII(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        for (int i = 1; i <= n; i++) {
+            a[i] = new PII(nextInt(), nextInt());
+        }
+
+        Arrays.sort(a, 1, n + 1, Comparator.comparingInt(a -> a.x));
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            f[i] = 1;
+            for (int j = 1; j < i; j++) {
+                if (a[i].y > a[j].y) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max(ans, f[i]);
+        }
+
+        out.println(ans);
+
+        close();
+    }
+
+    public static int nextInt() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+        return Integer.parseInt(tk.nextToken());
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
+}
---
+++ CPPSolutions/CPPSolutions/Q1012_友好城市.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+
+const int N = 5010;
+
+typedef pair<int, int> PII;
+
+PII a[N];
+int f[N];
+int n;
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) scanf("%d%d", &a[i].first, &a[i].second);
+
+	sort(a + 1, a + 1 + n);
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		f[i] = 1;
+		for (int j = 1; j < i; j++) if (a[i].second > a[j].second) f[i] = max(f[i], f[j] + 1);
+		ans = max(ans, f[i]);
+	}
+
+	printf("%d", ans);
+}
