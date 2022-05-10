ca7c8be60a0314c335cb8c9a832c44aef5e72462
Hyper18 <windhyperspace@gmail.com>
2022-05-10 18:16:04 +0800

    1024. 装箱问题

---
+++ CPPSolutions/CPPSolutions/Q1024_装箱问题.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include <iostream>
+
+const int N = 2e4 + 10;
+int f[N];
+int m, n;
+
+int main() {
+	scanf("%d%d", &m, &n);
+	for (int i = 1; i <= n; i++) {
+		int v;
+		scanf("%d", &v);
+		for (int j = m; j >= v; j--) f[j] = max(f[j], f[j - v] + v);
+	}
+	printf("%d", m - f[m]);
+}
