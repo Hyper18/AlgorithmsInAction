1ab26d4ae8614e8367b4926d4e8bc4e137c889a8
Hyper18 <windhyperspace@gmail.com>
2022-05-10 12:26:41 +0800

    1016. 最大上升子序列和

---
+++ CPPSolutions/CPPSolutions/Q1016_最大上升子序列和.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+
+const int N = 1010;
+int a[N], f[N];
+int n;
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		f[i] = a[i];
+		for (int j = 1; j < i; j++) if (a[i] > a[j]) f[i] = max(f[i], f[j] + a[i]);
+		ans = max(ans, f[i]);
+	}
+
+	printf("%d", ans);
+}
