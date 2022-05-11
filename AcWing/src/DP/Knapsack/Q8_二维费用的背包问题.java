eaa5adfa6e498d8cc3fd8ad0cc883c4fcf7cdb30
Hyper18 <windhyperspace@gmail.com>
2022-05-11 21:11:50 +0800

    8. 二维费用的背包问题 - Review

--- AcWing/src/DP/Knapsack/Q8_二维费用的背包问题.java
+++ AcWing/src/DP/Knapsack/Q8_二维费用的背包问题.java
@@ -12,26 +12,26 @@ public class Q8_二维费用的背包问题 {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer token = new StringTokenizer("");
     public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-    private static final int MAX = 100 + 10;
-    private static int[][] dp = new int[MAX][MAX];
+    private static final int N = 100 + 10;
+    private static int[][] f = new int[N][N];
 
     public static void main(String[] args) throws IOException {
-        int N = nextInt();
+        int n = nextInt();
         int V = nextInt();
         int M = nextInt();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             int v = nextInt();
             int m = nextInt();
             int w = nextInt();
 
             for (int j = V; j >= v; j--) {
                 for (int k = M; k >= m; k--) {
-                    dp[j][k] = Math.max(dp[j][k], dp[j - v][k - m] + w);
+                    f[j][k] = Math.max(f[j][k], f[j - v][k - m] + w);
                 }
             }
         }
 
-        out.println(dp[V][M]);
+        out.println(f[V][M]);
 
         close();
     }
