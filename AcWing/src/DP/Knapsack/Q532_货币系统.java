f6e4db17f45c06557bd08a721a2b790dc075ea42
Hyper18 <windhyperspace@gmail.com>
2022-05-11 21:08:19 +0800

    532. 货币系统

--- AcWing/src/DP/Knapsack/Q532_货币系统.java
+++ AcWing/src/DP/Knapsack/Q532_货币系统.java
@@ -13,14 +13,21 @@ import java.util.StringTokenizer;
  * 若当前方案能满足，则放入
  * 不满足，则不含在最优解内
  * <p>
- * 待回看
+ * 1. 状态表示
+ * 原题等价于 求出 货币系统 (n,a)的 最大无关向量组，即任意 ai 都不能被其他向量 线性表出
+ * 集合：f[i][j]表示对i个物品，当前体积为j的方案数
+ * 这里的背包体积应当等于最大币值，即数据量上限为 N = 25000
+ * 属性：当前方案是否成立
+ * 2. 状态计算
+ * 不选i（全不选，1） ~ 选n个i（不变， n）
+ * f[j] |= f[j - a[i]]
  */
 public class Q532_货币系统 {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer tk = new StringTokenizer("");
     public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
     private static final int N = 25000 + 10;
-    private static int[] v = new int[N];
+    private static int[] a = new int[N];
     private static boolean[] f = new boolean[N];
 
     public static void main(String[] args) throws IOException {
@@ -28,20 +35,20 @@ public class Q532_货币系统 {
         while (t-- > 0) {
             int n = nextInt();
             for (int i = 0; i < n; i++) {
-                v[i] = nextInt();
+                a[i] = nextInt();
             }
 
-            Arrays.sort(v, 0, n);
+            Arrays.sort(a, 0, n);
             // 每次应重新初始化
             Arrays.fill(f, false);
             f[0] = true;
-            int ans = 0, m = v[n - 1];
+            int ans = 0, m = a[n - 1];
             for (int i = 0; i < n; i++) {
-                if (!f[v[i]]) {
+                if (!f[a[i]]) {
                     ans++;
                 }
-                for (int j = v[i]; j <= m; j++) {
-                    f[j] |= f[j - v[i]];
+                for (int j = a[i]; j <= m; j++) {
+                    f[j] |= f[j - a[i]];
                 }
             }
             out.println(ans);
---
+++ CPPSolutions/CPPSolutions/Q532_货币系统.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+const int N = 25010;
+int a[N];
+bool f[N];
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+		sort(a, a + n);
+		memset(f, 0, sizeof f);
+		f[0] = 1;
+		int ans = 0, m = a[n - 1];
+		for (int i = 0; i < n; i++) {
+			if (!f[a[i]]) ans++;
+			for (int j = a[i]; j <= m; j++) {
+				f[j] |= f[j - a[i]];
+			}
+		}
+		printf("%d\n", ans);
+	}
+}
