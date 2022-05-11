6ef9ccea711608ee42743e62536c431fa808ff32
Hyper18 <windhyperspace@gmail.com>
2022-05-11 21:16:48 +0800

    8. 二维费用的背包问题

---
+++ CPPSolutions/CPPSolutions/Q8_二维费用的背包问题.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+
+const int N = 110;
+int f[N][N];
+int n, V, M;
+
+int main() {
+	scanf("%d%d%d", &n, &V, &M);
+	for (int i = 0; i < n; i++) {
+		int v, m, w;
+		scanf("%d%d%d", &v, &m, &w);
+		for (int j = V; j >= v; j--) {
+			for (int k = M; k >= m; k--) {
+				f[j][k] = max(f[j][k], f[j - v][k - m] + w);
+			}
+		}
+	}
+	printf("%d", f[V][M]);
+}
