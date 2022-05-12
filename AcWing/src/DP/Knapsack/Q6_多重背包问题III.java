f47a2d69c666ea9c880a50e0217c61c50a9a3ea9
Hyper18 <windhyperspace@gmail.com>
2022-05-12 10:49:59 +0800

    6. 多重背包问题 III - 单调队列优化

--- AcWing/src/DP/Knapsack/Q6_多重背包问题III.java
+++ AcWing/src/DP/Knapsack/Q6_多重背包问题III.java
@@ -1,8 +1,6 @@
 package DP.Knapsack;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -10,50 +8,39 @@ import java.util.StringTokenizer;
  * @date 2022/03/18
  * <p>
  * 多重背包问题的单调队列优化
- * to-do，有空补 这里用二的方法java是可以过的hh
+ * <p>
+ * 待回看
  */
 public class Q6_多重背包问题III {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer token = new StringTokenizer("");
     public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-    private static final int MAX = 20000 + 10;
-    private static int[] dp = new int[MAX];
-
-    static class Good {
-        int v;
-        int w;
-
-        public Good(int v, int w) {
-            this.v = v;
-            this.w = w;
-        }
-    }
+    private static final int N = 20000 + 10;
+    private static int[] f = new int[N], g = new int[N];
+    private static int[] q = new int[N];
 
     public static void main(String[] args) throws IOException {
-        int N = nextInt();
-        int V = nextInt();
+        int n = nextInt(), m = nextInt();
 
-        List<Good> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            int v = nextInt();
-            int w = nextInt();
-            int s = nextInt();
-            for (int k = 1; k <= s; k <<= 1) {
-                s -= k;
-                list.add(new Good(v * k, w * k));
-            }
-            if (s > 0) {
-                list.add(new Good(v * s, w * s));
-            }
-        }
-
-        for (Good good : list) {
-            for (int j = V; j >= good.v; j--) {
-                dp[j] = Math.max(dp[j], dp[j - good.v] + good.w);
+        for (int i = 0; i < n; i++) {
+            int v = nextInt(), w = nextInt(), s = nextInt();
+            System.arraycopy(f, 0, g, 0, f.length);
+            for (int j = 0; j < v; j++) {
+                int hh = 0, tt = -1;
+                for (int k = j; k <= m; k += v) {
+                    if (hh <= tt && q[hh] < k - s * v) {
+                        hh++;
+                    }
+                    while (hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w) {
+                        tt--;
+                    }
+                    q[++tt] = k;
+                    f[k] = g[q[hh]] + (k - q[hh]) / v * w;
+                }
             }
         }
 
-        out.println(dp[V]);
+        out.println(f[m]);
 
         close();
     }
---
+++ CPPSolutions/CPPSolutions/Q6_多重背包问题III.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+const int N = 20010;
+
+int f[N], g[N], q[N];
+int n, m;
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        int v, w, s;
+        scanf("%d%d%d", &v, &w, &s);
+        memcpy(g, f, sizeof f);
+        for (int j = 0; j < v; j++) {
+            int hh = 0, tt = -1;
+            for (int k = j; k <= m; k += v) {
+                if (hh <= tt && q[hh] < k - s * v) hh++;
+                while (hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w) tt--;
+                q[++tt] = k;
+                f[k] = g[q[hh]] + (k - q[hh]) / v * w;
+            }
+        }
+    }
+    printf("%d", f[m]);
+}
