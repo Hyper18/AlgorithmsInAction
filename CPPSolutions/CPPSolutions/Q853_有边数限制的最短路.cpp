fe5f085dc2bc8abc5d2799787c5723910e71b0fe
Hyper18 <windhyperspace@gmail.com>
2022-05-29 21:15:14 +0800

    853. 有边数限制的最短路

---
+++ CPPSolutions/CPPSolutions/Q853_有边数限制的最短路.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include <iostream>
+#include <cstring>
+
+const int N = 510, M = 1e4 + 10, INF = 0x3f3f3f3f;
+
+int n, m, k;
+int d[N], t[N];
+
+struct Edge {
+	int a, b, c;
+}edges[M];
+
+void bellmanFord() {
+	memset(d, INF, sizeof d);
+	d[1] = 0;
+
+	for (int i = 0; i < k; i++) {
+		memcpy(t, d, sizeof d);
+		for (int j = 0; j < m; j++) {
+			auto e = edges[j];
+			d[e.b] = min(d[e.b], t[e.a] + e.c);
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d%d", &n, &m, &k);
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		edges[i] = { a, b, c };
+	}
+	bellmanFord();
+	if (d[n] > INF / 2)
+		printf("impossible");
+	else printf("%d", d[n]);
+}
