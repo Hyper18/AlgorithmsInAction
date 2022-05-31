b884c9483f62f0d3cc5340a43b5e5a2d95defb59
Hyper18 <windhyperspace@gmail.com>
2022-05-31 11:05:43 +0800

    837. 连通块中点的数量

---
+++ CPPSolutions/CPPSolutions/Q837_连通块中点的数量.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include <iostream>
+
+const int N = 1e5 + 10;
+
+int a, b;
+string op;
+int p[N], cnt[N];
+
+int find(int x) {
+	if (x != p[x]) p[x] = find(p[x]);
+	return p[x];
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		p[i] = i;
+		cnt[i]++;
+	}
+	while (m--) {
+		cin >> op >> a;
+		if (op == "C") {
+			cin >> b;
+			int p1 = find(a), p2 = find(b);
+			if (p1 != p2) {
+				p[p1] = p2;
+				cnt[p2] += cnt[p1];
+			}
+		}
+		else if (op == "Q1") {
+			cin >> b;
+			printf(find(a) == find(b) ? "Yes\n" : "No\n");
+		}
+		else printf("%d\n", cnt[find(a)]);
+	}
+}
