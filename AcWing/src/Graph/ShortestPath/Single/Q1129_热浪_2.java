500e27b487f42b9e0672e6adc3e8d4fd9d001932
Hyper18 <windhyperspace@gmail.com>
2022-06-01 21:17:23 +0800

    1129. 热浪 写法二

---
+++ AcWing/src/Graph/ShortestPath/Single/Q1129_热浪_2.java
@@ -0,0 +1,79 @@
+package Graph.ShortestPath.Single;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * @author Hyperspace
+ * @date 2022/06/01
+ * 使用内置API的写法
+ */
+public class Q1129_热浪_2 {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer tk = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    private static final int N = 2500 + 10, M = 6200 * 2 + 10, INF = 0x3f3f3f3f;
+    private static int[] h = new int[N], e = new int[M], ne = new int[M], w = new int[M];
+    private static int[] d = new int[N];
+    private static boolean[] vis = new boolean[N];
+    static int idx;
+
+    private static void addEdge(int a, int b, int c) {
+        e[idx] = b;
+        w[idx] = c;
+        ne[idx] = h[a];
+        h[a] = idx++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt(), m = nextInt(), st = nextInt(), ed = nextInt();
+        Arrays.fill(h, -1);
+        while (m-- > 0) {
+            int a = nextInt(), b = nextInt(), c = nextInt();
+            addEdge(a, b, c);
+            addEdge(b, a, c);
+        }
+        spfa(st);
+        out.println(d[ed]);
+        close();
+    }
+
+    private static void spfa(int st) {
+        Arrays.fill(d, INF);
+        d[st] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(st);
+        vis[st] = true;
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            vis[u] = false;
+            for (int i = h[u]; i != -1; i = ne[i]) {
+                int j = e[i];
+                if (d[j] > d[u] + w[i]) {
+                    d[j] = d[u] + w[i];
+                    if (vis[j]) {
+                        continue;
+                    }
+                    vis[j] = true;
+                    q.offer(j);
+                }
+            }
+        }
+    }
+
+    public static int nextInt() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+        return Integer.parseInt(tk.nextToken());
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
+}
---
+++ CPPSolutions/CPPSolutions/Q1129_热浪_2.cpp
@@ -0,0 +1,50 @@
+using namespace std;
+#include <iostream>
+#include <cstring>
+#include <queue>
+
+const int N = 2510, M = 6200 * 2 + 10, INF = 0x3f3f3f3f;
+
+int n, m, st, ed;
+int h[N], e[M], ne[M], w[M], idx;
+int d[N];
+bool vis[N];
+
+void addEdge(int a, int b, int c) {
+	e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+void spfa() {
+	memset(d, INF, sizeof d);
+	d[st] = 0;
+	
+	queue<int> q;
+	q.push(st);
+	vis[st] = true;
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		vis[u] = false;
+		for (int i = h[u]; i != -1; i = ne[i]) {
+			int j = e[i];
+			if (d[j] > d[u] + w[i]) {
+				d[j] = d[u] + w[i];
+				if (vis[j]) continue;
+				vis[j] = true;
+				q.push(j);
+			}
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d%d%d", &n, &m, &st, &ed);
+	memset(h, -1, sizeof h);
+	while (m--) {
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		addEdge(a, b, c), addEdge(b, a, c);
+	}
+	spfa();
+	printf("%d", d[ed]);
+}
