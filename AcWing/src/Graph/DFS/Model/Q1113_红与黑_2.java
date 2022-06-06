5b318c4b1107fceea7f5fd89a5fc1566de9446c7
Hyper18 <windhyperspace@gmail.com>
2022-06-06 20:03:24 +0800

    1113. 红与黑 - 法二

---
+++ AcWing/src/Graph/DFS/Model/Q1113_红与黑_2.java
@@ -0,0 +1,77 @@
+package Graph.DFS.Model;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * @author Hyperspace
+ * @date 2022/06/06
+ * <p>
+ * 思路
+ * 每次在递归函数出口累加并记录访问数目
+ */
+public class Q1113_红与黑_2 {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer tk = new StringTokenizer("");
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    private static final int N = 20 + 3;
+    private static final int[] dx = new int[]{-1, 0, 1, 0}, dy = new int[]{0, -1, 0, 1};
+    private static char[][] g = new char[N][N];
+    private static boolean[][] vis;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            n = nextInt();
+            m = nextInt();
+            if (n == 0 && m == 0) {
+                close();
+                System.exit(0);
+            }
+            int sx = 0, sy = 0;
+            for (int i = 0; i < m; i++) {
+                String input = next();
+                g[i] = input.toCharArray();
+                if (input.indexOf('@') != -1) {
+                    sx = i;
+                    sy = input.indexOf('@');
+                }
+            }
+            vis = new boolean[N][N];
+            out.println(dfs(sx, sy));
+        }
+    }
+
+    private static int dfs(int x, int y) {
+        int ans = 1;
+        vis[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n || vis[nx][ny] || g[nx][ny] == '#') {
+                continue;
+            }
+            ans += dfs(nx, ny);
+        }
+
+        return ans;
+    }
+
+    public static int nextInt() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+        return Integer.parseInt(tk.nextToken());
+    }
+
+    public static String next() throws IOException {
+        while (!tk.hasMoreTokens()) {
+            tk = new StringTokenizer(in.readLine());
+        }
+        return tk.nextToken();
+    }
+
+    public static void close() throws IOException {
+        in.close();
+        out.close();
+    }
+}
