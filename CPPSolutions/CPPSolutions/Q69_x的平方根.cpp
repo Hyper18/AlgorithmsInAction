4fd72927b0c2afa7a1be8dcf2c4e1270facf3f9a
Hyper18 <windhyperspace@gmail.com>
2022-10-10 10:46:39 +0800

    69. x 的平方根

---
+++ CPPSolutions/CPPSolutions/Q69_x的平方根.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        int ans = exp(0.5 * log(x));
+        return ((long long)(ans + 1) * (ans + 1) <= x ? ans + 1 : ans);
+    }
+
+    int mySqrt2(int x) {
+        int low = 0, high = x, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((long long)mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
---
+++ LeetCode/src/BinarySearch/Q69_x的平方根.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+/**
+ * @author Hyperspace
+ * @date 2022/10/10
+ * <p>
+ * 思路
+ * 1. 数学做法，先转化为以e为底数再套用Math工具类解决
+ * 2. 二分
+ */
+public class Q69_x的平方根 {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.exp(0.5 * Math.log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+
+    public int mySqrt2(int x) {
+        int low = 0, high = x;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // 需要注意溢出的情况
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
