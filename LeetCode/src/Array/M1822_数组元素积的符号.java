898e531434c6ef021a9558adb370820dc72e2339
Hyper18 <windhyperspace@gmail.com>
2022-10-27 13:12:06 +0800

    27th, Oct

---
+++ LeetCode/src/Array/M1822_数组元素积的符号.java
@@ -0,0 +1,20 @@
+package Array;
+
+/**
+ * @author Hyperspace
+ * @date 2022/10/27
+ */
+public class M1822_数组元素积的符号 {
+    public int arraySign(int[] nums) {
+        int ans = 1;
+        for (int n : nums) {
+            if (n == 0) {
+                return 0;
+            } else if (n < 0) {
+                ans = -ans;
+            }
+        }
+
+        return ans;
+    }
+}
