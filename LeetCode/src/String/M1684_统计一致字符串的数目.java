11e74e69bb194e3cf0f7d957a2fc029b79e30798
Hyper18 <windhyperspace@gmail.com>
2022-11-08 11:22:46 +0800

    8th, Nov

---
+++ LeetCode/src/String/M1684_统计一致字符串的数目.java
@@ -0,0 +1,24 @@
+package String;
+
+/**
+ * @author Hyperspace
+ * @date 2022/11/08
+ * <p>
+ * 思路
+ * 暴力
+ */
+public class M1684_统计一致字符串的数目 {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int ans = words.length;
+        for (String w : words) {
+            for (char c : w.toCharArray()) {
+                if (allowed.indexOf(c) < 0) {
+                    ans--;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
