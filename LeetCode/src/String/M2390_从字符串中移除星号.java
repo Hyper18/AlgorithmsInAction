d6436fd66c7d24d78fb2b943ff1c6b1875752b48
Hyper18 <windhyperspace@gmail.com>
2024-09-14 20:14:45 +0800

    14th, Sep

---
+++ LeetCode/src/String/M2390_从字符串中移除星号.java
@@ -0,0 +1,23 @@
+package String;
+
+/**
+ * @author Hyperspace
+ * @date 2024/09/14
+ * @file M2390_从字符串中移除星号.java
+ */
+public class M2390_从字符串中移除星号 {
+    public String removeStars(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '*') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
