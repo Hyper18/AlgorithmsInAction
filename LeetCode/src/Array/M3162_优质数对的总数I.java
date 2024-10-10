6a0e627a45236e38d69d8ba0071feee9ad4bef6f
Hyper18 <windhyperspace@gmail.com>
2024-10-10 09:39:56 +0800

    10th, Oct

---
+++ LeetCode/src/Array/M3162_优质数对的总数I.java
@@ -0,0 +1,21 @@
+package Array;
+
+/**
+ * @author Hyperspace
+ * @date 2024/10/10
+ * @file M3162_优质数对的总数I.java
+ */
+public class M3162_优质数对的总数I {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int ans = 0;
+        for (int x : nums1) {
+            for (int y : nums2) {
+                if (x % (y * k) == 0) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
