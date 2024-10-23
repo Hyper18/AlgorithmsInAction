2110c0a599c0db2ccfe28e544cd3763a39b198b8
Hyper18 <windhyperspace@gmail.com>
2024-10-23 11:00:11 +0800

    23rd, Oct

---
+++ LeetCode/src/Array/M3185_构成整天的下标对数目II.java
@@ -0,0 +1,37 @@
+package Array;
+
+/**
+ * @author Hyperspace
+ * @date 2024/10/23
+ * @file M3185_构成整天的下标对数目II.java
+ * <p>
+ * 思路
+ * 1. 模拟 O(n^2) TLE
+ * 2. 优化 O(n)
+ */
+public class M3185_构成整天的下标对数目II {
+    public long countCompleteDayPairs(int[] hours) {
+        int n = hours.length;
+        long ans = 0;
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                if ((hours[i] + hours[j]) % 24 == 0) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public long countCompleteDayPairs2(int[] hours) {
+        int[] cnt = new int[24];
+        long ans = 0;
+        for (int h : hours) {
+            ans += cnt[(24 - h % 24) % 24];
+            cnt[h % 24]++;
+        }
+
+        return ans;
+    }
+}
