54c4b2512e49e47e64572ef1d783d95f448bc99f
Hyper18 <windhyperspace@gmail.com>
2024-11-12 11:19:48 +0800

    12th, Nov

---
+++ LeetCode/src/SWM/M3258_统计满足K约束的子字符串数量I.java
@@ -0,0 +1,66 @@
+package SWM;
+
+/**
+ * @author Hyperspace
+ * @date 2024/11/12
+ * @file M3258_统计满足K约束的子字符串数量I.java
+ * <p>
+ * 思路
+ * 1. 模拟 O(n^2)
+ * 2. SWM
+ * 3. 位运算优化
+ * 假设当前值为x
+ * 由于字符 0 的 ASCII 值是偶数，字符 1 的 ASCII 值是奇数
+ * (1) ascii(x) AND 1 = 0 x为偶数
+ * (2) ascii(x) AND 1 = 1 x为奇数
+ */
+public class M3258_统计满足K约束的子字符串数量I {
+    public int countKConstraintSubstrings(String s, int k) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int[] cnt = new int[2];
+            cnt[s.charAt(i) - '0']++;
+            ans++;
+            for (int j = i + 1; j < n; j++) {
+                cnt[s.charAt(j) - '0']++;
+                if (cnt[0] > k && cnt[1] > k) {
+                    break;
+                }
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    public int countKConstraintSubstrings2(String s, int k) {
+        int n = s.length();
+        int[] cnt = new int[2];
+        int ans = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            cnt[s.charAt(r) - '0']++;
+            while (cnt[0] > k && cnt[1] > k) {
+                cnt[s.charAt(l++) - '0']--;
+            }
+            ans += r - l + 1;
+        }
+
+        return ans;
+    }
+
+    public int countKConstraintSubstrings3(String s, int k) {
+        int n = s.length();
+        int[] cnt = new int[2];
+        int ans = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            cnt[s.charAt(r) & 1]++;
+            while (cnt[0] > k && cnt[1] > k) {
+                cnt[s.charAt(l++) & 1]--;
+            }
+            ans += r - l + 1;
+        }
+
+        return ans;
+    }
+}
---
+++ LeetCode/src/SWM/M3261_统计满足K约束的子字符串数量II.java
@@ -0,0 +1,80 @@
+package SWM;
+
+/**
+ * @author Hyperspace
+ * @date 2024/11/12
+ * @file M3261_统计满足K约束的子字符串数量II.java
+ * <p>
+ * 思路
+ * 1. SWM TLE
+ * 2. SWM+前缀和+二分
+ * 两种二分的写法都是可行的
+ * <p>
+ * 注意前缀和计算不要爆int
+ */
+public class M3261_统计满足K约束的子字符串数量II {
+    public long[] countKConstraintSubstrings(String s, int k, int[][] queries) {
+        int m = queries.length;
+        long[] res = new long[m];
+        char[] cs = s.toCharArray();
+        for (int i = 0; i < m; i++) {
+            int[] cnt = new int[2];
+            for (int l = queries[i][0], r = queries[i][0]; r <= queries[i][1]; r++) {
+                cnt[cs[r] - '0']++;
+                while (cnt[0] > k && cnt[1] > k) {
+                    cnt[cs[l++] - '0']--;
+                }
+                res[i] += r - l + 1;
+            }
+        }
+
+        return res;
+    }
+
+    public long[] countKConstraintSubstrings2(String s, int k, int[][] queries) {
+        int n = s.length(), m = queries.length;
+        int[] cnt = new int[2], pos = new int[n];
+        long[] pre = new long[n + 1];
+        for (int l = 0, r = 0; r < n; r++) {
+            cnt[s.charAt(r) - '0']++;
+            while (cnt[0] > k && cnt[1] > k) {
+                cnt[s.charAt(l++) - '0']--;
+            }
+            pos[r] = l;
+            pre[r + 1] = pre[r] + r - l + 1;
+        }
+        long[] res = new long[m];
+        for (int i = 0; i < m; i++) {
+            int ql = queries[i][0], qr = queries[i][1], mid = binarySearch(pos, ql, qr + 1, ql), len = mid - ql;
+            res[i] = pre[qr + 1] - pre[mid] + (long) (1 + len) * len / 2;
+        }
+
+        return res;
+    }
+
+    private int binarySearch(int[] pos, int low, int high, int target) {
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (pos[mid] >= target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private int binarySearch2(int[] pos, int low, int high, int target) {
+        while (low + 1 < high) {
+            int mid = low + ((high - low) >> 1);
+            if (pos[mid] >= target) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return high;
+    }
+}
