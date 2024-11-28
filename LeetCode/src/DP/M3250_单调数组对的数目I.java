58bf1d245fb7d50483c5ef3be7b9b7bb9012b4c4
Hyper18 <windhyperspace@gmail.com>
2024-11-28 15:17:49 +0800

    28th, Nov

---
+++ LeetCode/src/DP/M3250_单调数组对的数目I.java
@@ -0,0 +1,34 @@
+package DP;
+
+import java.util.Arrays;
+
+/**
+ * @author Hyperspace
+ * @date 2024/11/28
+ * @file M3250_单调数组对的数目I.java
+ * <p>
+ * 思路@灵茶山艾府
+ * 前缀和+DP
+ */
+public class M3250_单调数组对的数目I {
+    final int MOD = (int) 1e9 + 7;
+
+    public int countOfPairs(int[] nums) {
+        int n = nums.length, m = Arrays.stream(nums).max().getAsInt();
+        long[][] f = new long[n][m + 1];
+        long[] pre = new long[m + 1];
+        Arrays.fill(f[0], 0, nums[0] + 1, 1);
+        for (int i = 1; i < n; i++) {
+            pre[0] = f[i - 1][0];
+            for (int k = 1; k <= m; k++) {
+                pre[k] = (pre[k - 1] + f[i - 1][k]) % MOD;
+            }
+            for (int j = 0; j <= nums[i]; j++) {
+                int maxK = j + Math.min(nums[i - 1] - nums[i], 0);
+                f[i][j] = maxK >= 0 ? pre[maxK] % MOD : 0;
+            }
+        }
+
+        return (int) (Arrays.stream(f[n - 1], 0, nums[n - 1] + 1).sum() % MOD);
+    }
+}
