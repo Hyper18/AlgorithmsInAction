c42142179579e0709fd1e8416ca280c0bfe59da5
Hyper18 <windhyperspace@gmail.com>
2024-12-11 17:50:04 +0800

    11th, Dec

---
+++ KamaCoder/src/Week21/Q132_小美的树上染色.java
@@ -0,0 +1,78 @@
+package Week21;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2024/12/11
+ * @file Q132_小美的树上染色.java
+ * <p>
+ * 题目描述
+ * 小美拿到了一棵树，每个节点有一个权值。初始每个节点都是白色。
+ * 小美有若干次操作，每次操作可以选择两个相邻的节点，如果它们都是白色且权值的乘积是完全平方数，小美就可以把这两个节点同时染红。
+ * 小美想知道，自己最多可以染红多少个节点？
+ * <p>
+ * 输入描述
+ * 第一行输入一个正整数 n（1 <= n <= 10^5），代表节点的数量。
+ * 第二行输入 n 个正整数 ai（1 <= ai <= 10^9），代表每个节点的权值。
+ * 接下来的 n - 1 行，每行输入两个正整数 u，v（1 <= u, v <= n），代表节点 u 和节点 v 有一条边连接。
+ * <p>
+ * 输出描述
+ * 输出一个整数，表示最多可以染红的节点数量。
+ * <p>
+ * 思路@kamaCoder29697
+ * 树形DP
+ */
+public class Q132_小美的树上染色 {
+    private static final int N = (int) 1e5 + 10;
+    private static Node[] nodes = new Node[N];
+    private static int[][] f = new int[N][2];
+
+    static class Node {
+        long val;
+        List<Integer> childs;
+
+        public Node(long val) {
+            this.val = val;
+            this.childs = new ArrayList<>();
+        }
+    }
+
+    private static boolean judge(int a, int b) {
+        long ans = nodes[a].val * nodes[b].val, sqr = (int) Math.sqrt(ans);
+        return ans == sqr * sqr;
+    }
+
+    private static void dfs(int u, int fa) {
+        for (int c : nodes[u].childs) {
+            if (c == fa) {
+                continue;
+            }
+            dfs(c, u);
+            f[u][0] += Math.max(f[c][0], f[c][1]);
+        }
+        for (int c : nodes[u].childs) {
+            if (c == fa || !judge(u, c)) {
+                continue;
+            }
+            f[u][1] = Math.max(f[u][1], f[u][0] - Math.max(f[c][0], f[c][1]) + 2 + f[c][0]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new Node(sc.nextInt());
+        }
+        while (--n > 0) {
+            int u = sc.nextInt(), v = sc.nextInt();
+            nodes[u].childs.add(v);
+            nodes[v].childs.add(u);
+        }
+        dfs(1, 0);
+        System.out.println(Math.max(f[1][0], f[1][1]));
+    }
+}
---
+++ KamaCoder/src/Week22/Q133_夹吃棋.java
@@ -0,0 +1,81 @@
+package Week22;
+
+import java.util.Scanner;
+
+/**
+ * @author Hyperspace
+ * @date 2024/12/11
+ * @file Q133_夹吃棋.java
+ * <p>
+ * 题目描述
+ * 在一个 3 * 3 的棋盘上，小红和小紫正在玩“夹吃棋”。
+ * 所谓“夹吃棋”，即如果存在一个白子，它的两侧 (横向或者纵向)相邻都是黑子，则这个棋子将被“夹吃”，对于黑棋亦然。
+ * 如果一个棋盘的局面没有一方被夹吃，或者黑白双方都被对面夹吃，则认为是平局。
+ * 如果只有一方夹吃了另一方，则认为夹吃方赢，被夹吃方输。
+ * 小红执黑棋，小紫执白棋，现在给定一个局面，请你判断当前棋局是谁获胜。
+ * <p>
+ * 输入描述
+ * 第一行输入一个正整数 t （1 <= t <= 10000），代表询问的次数。
+ * 接下来每组询问输入三行，，每行是一个长度为3的字符串，字符串仅由'o','.','*'组成。
+ * 其中 o 代表白棋，* 代表黑棋，. 代表未放置棋子。
+ * <p>
+ * 输出描述
+ * 小红获胜输出“kou”，小紫获胜输出“yukan”，平局输出“draw”。
+ * <p>
+ * 思路
+ * 模拟
+ */
+public class Q133_夹吃棋 {
+    private static final int N = 3;
+    private static char[][] g;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            g = new char[N][N];
+            for (int i = 0; i < N; i++) {
+                g[i] = sc.next().toCharArray();
+            }
+            play();
+        }
+    }
+
+    private static void play() {
+        boolean f1 = false, f2 = false;
+        for (int i = 0; i < N && (!f1 || !f2); i++) {
+            for (int j = 0; j < N && (!f1 || !f2); j++) {
+                if (g[i][j] == '.') {
+                    continue;
+                }
+                if (check(i - 1, j) && check(i + 1, j) && g[i][j] != g[i - 1][j] && g[i][j] != g[i + 1][j]) {
+                    if (!f1) {
+                        f1 = g[i][j] == 'o';
+                    }
+                    if (!f2) {
+                        f2 = g[i][j] == '*';
+                    }
+                }
+                if (check(i, j - 1) && check(i, j + 1) && g[i][j] != g[i][j - 1] && g[i][j] != g[i][j + 1]) {
+                    if (!f1) {
+                        f1 = g[i][j] == 'o';
+                    }
+                    if (!f2) {
+                        f2 = g[i][j] == '*';
+                    }
+                }
+            }
+        }
+        if ((f1 && f2) || (!f1 && !f2)) {
+            System.out.println("draw");
+        } else if (f1) {
+            System.out.println("kou");
+        } else {
+            System.out.println("yukan");
+        }
+    }
+
+    private static boolean check(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N && g[x][y] != '.';
+    }
+}
