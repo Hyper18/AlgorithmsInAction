e1e0935a05a8a3c9589d719e7df261144c5b2cdf
Hyper18 <windhyperspace@gmail.com>
2024-12-28 21:06:11 +0800

    28th, Dec

---
+++ LeetCode/src/Array/M3046_分割数组.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @author Hyperspace
+ * @date 2024/12/28
+ * @file M3046_分割数组.java
+ */
+public class M3046_分割数组 {
+    public boolean isPossibleToSplit(int[] nums) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int num : nums) {
+            mp.merge(num, 1, Integer::sum);
+            if (mp.get(num) > 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPossibleToSplit2(int[] nums) {
+        int[] cnt = new int[101];
+        for (int num : nums) {
+            if (++cnt[num] > 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
