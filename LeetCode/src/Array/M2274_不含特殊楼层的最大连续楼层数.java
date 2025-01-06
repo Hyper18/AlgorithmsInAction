7c09fa690b82ed4349315a694f2c14e7edc27543
Hyper18 <windhyperspace@gmail.com>
2025-01-06 10:35:30 +0800

    6th, Jan

---
+++ LeetCode/src/Array/M2274_不含特殊楼层的最大连续楼层数.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Arrays;
+
+/**
+ * @author Hyperspace
+ * @date 2025/01/06
+ * @file M2274_不含特殊楼层的最大连续楼层数.java
+ */
+public class M2274_不含特殊楼层的最大连续楼层数 {
+    public int maxConsecutive(int bottom, int top, int[] special) {
+        Arrays.sort(special);
+        int ans = 0, l = bottom;
+        for (int r : special) {
+            ans = Math.max(ans, r - l);
+            l = r + 1;
+        }
+
+        return Math.max(ans, top - l + 1);
+    }
+}
