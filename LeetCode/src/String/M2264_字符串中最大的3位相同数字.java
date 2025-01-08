17de9685727cdb613c06cea7fa0292b542c503a8
Hyper18 <windhyperspace@gmail.com>
2025-01-08 16:08:00 +0800

    8th, Jan

---
+++ LeetCode/src/String/M2264_字符串中最大的3位相同数字.java
@@ -0,0 +1,20 @@
+package String;
+
+/**
+ * @author Hyperspace
+ * @date 2025/01/08
+ * @file M2264_字符串中最大的3位相同数字.java
+ */
+public class M2264_字符串中最大的3位相同数字 {
+    private final String[] nums = new String[]{"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+
+    public String largestGoodInteger(String num) {
+        for (int i = 0; i < 10; i++) {
+            if (num.contains(nums[i])) {
+                return nums[i];
+            }
+        }
+
+        return "";
+    }
+}
