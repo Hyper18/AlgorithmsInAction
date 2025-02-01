0a2052e24ed037588ac434ff643f5f7a12be34e9
Hyper18 <windhyperspace@gmail.com>
2025-02-01 15:05:29 +0800

    1st, Feb

---
+++ LeetCode/src/Array/Q81_搜索旋转排序数组II.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+
+/**
+ * @author Hyperspace
+ * @date 2025/02/01
+ * @file Q81_搜索旋转排序数组II.java
+ */
+public class Q81_搜索旋转排序数组II {
+    public boolean search(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean search2(int[] nums, int target) {
+        return Arrays.stream(nums).anyMatch(e -> e == target);
+    }
+}
