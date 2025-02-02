b8e2ac77b708559af066ed3a542939599c01aab3
Hyper18 <windhyperspace@gmail.com>
2025-02-02 14:37:44 +0800

    2nd, Feb

---
+++ LeetCode/src/Matrix/M598_区间加法II.java
@@ -0,0 +1,18 @@
+package Matrix;
+
+/**
+ * @author Hyperspace
+ * @date 2025/02/02
+ * @file M598_区间加法II.java
+ */
+public class M598_区间加法II {
+    public int maxCount(int m, int n, int[][] ops) {
+        int x = m, y = n;
+        for (int[] op : ops) {
+            x = Math.min(x, op[0]);
+            y = Math.min(y, op[1]);
+        }
+
+        return x * y;
+    }
+}
