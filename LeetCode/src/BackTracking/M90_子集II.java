e2f89afa0cc6b3f1770cc1d01d1e16954f05565f
Hyper18 <windhyperspace@gmail.com>
2025-02-05 11:39:42 +0800

    5th, Feb

---
+++ LeetCode/src/BackTracking/M90_子集II.java
@@ -0,0 +1,35 @@
+package BackTracking;
+
+import java.util.*;
+
+/**
+ * @author Hyperspace
+ * @date 2025/02/05
+ * @file M90_子集II.java
+ * <p>
+ * 思路
+ * 回溯
+ */
+public class M90_子集II {
+    private int[] nums;
+    private List<Integer> path = new ArrayList<>();
+    private Set<List<Integer>> st = new HashSet<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        this.nums = nums;
+        dfs(0);
+        return new ArrayList<>(st);
+    }
+
+    private void dfs(int i) {
+        if (i == nums.length) {
+            st.add(new ArrayList<>(path));
+            return;
+        }
+        dfs(i + 1);
+        path.add(nums[i]);
+        dfs(i + 1);
+        path.remove(path.size() - 1);
+    }
+}
---
+++ LeetCode/src/BackTracking/M90_子集II2.java
@@ -0,0 +1,43 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Hyperspace
+ * @date 2025/02/05
+ * @file M90_子集II.java
+ * <p>
+ * 思路
+ * 回溯
+ */
+public class M90_子集II2 {
+    private int[] nums;
+    private List<Integer> path = new ArrayList<>();
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        this.nums = nums;
+        dfs(0);
+        return res;
+    }
+
+    private void dfs(int i) {
+        int n = nums.length;
+        if (i == n) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        int x = nums[i];
+        path.add(x);
+        dfs(i + 1);
+        path.remove(path.size() - 1);
+        i++;
+        while (i < n && nums[i] == x) {
+            i++;
+        }
+        dfs(i);
+    }
+}
