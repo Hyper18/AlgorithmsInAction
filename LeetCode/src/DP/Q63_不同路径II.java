004eeda440e6b5c0f64cbfd262f6d2bf4f3da8e4
Hyper18 <windhyperspace@gmail.com>
2025-02-08 21:24:45 +0800

    8th, Feb

--- LeetCode/src/DP/Q63_不同路径II.java
+++ LeetCode/src/DP/Q63_不同路径II.java
@@ -2,7 +2,7 @@ package DP;
 
 /**
  * @author Hyper
- * @date 2024/09/23
+ * @date 2024/09/23，2025/02/08
  * @file Q63_不同路径II.java
  * <p>
  * 思路
@@ -26,28 +26,20 @@ public class Q63_不同路径II {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] f = new int[m][n];
-        f[0][0] = check(obstacleGrid, 0, 0) ? 0 : 1;
+        f[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
         for (int j = 1; j < n; j++) {
-            f[0][j] = check(obstacleGrid, 0, j) ? 0 : f[0][j - 1];
+            f[0][j] = obstacleGrid[0][j] == 1 ? 0 : f[0][j - 1];
         }
         for (int i = 1; i < m; i++) {
-            f[i][0] = check(obstacleGrid, i, 0) ? 0 : f[i - 1][0];
+            f[i][0] = obstacleGrid[i][0] == 1 ? 0 : f[i - 1][0];
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (check(obstacleGrid, i, j)) {
-                    f[i][j] = 0;
-                    continue;
-                }
-                f[i][j] = f[i - 1][j] + f[i][j - 1];
+                f[i][j] = obstacleGrid[i][j] == 1 ? 0 : f[i - 1][j] + f[i][j - 1];
             }
         }
 
         return f[m - 1][n - 1];
-    }
-
-    private boolean check(int[][] a, int i, int j) {
-        return a[i][j] == 1;
     }
 
     public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
