d33adaad9b3333ec25b888b8809c9966aa40e336
Hyper18 <windhyperspace@gmail.com>
2025-03-24 00:03:13 +0800

    24th, Mar

---
+++ LeetCode/src/String/M2255_统计是给定字符串前缀的字符串数目.java
@@ -0,0 +1,33 @@
+package String;
+
+/**
+ * @author Hyperspace
+ * @date 2025/03/24
+ * @file M2255_统计是给定字符串前缀的字符串数目.java
+ * <p>
+ * 思路
+ * 调库
+ */
+public class M2255_统计是给定字符串前缀的字符串数目 {
+    public int countPrefixes(String[] words, String s) {
+        int ans = 0;
+        for (String word : words) {
+            if (s.indexOf(word) == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    public int countPrefixes2(String[] words, String s) {
+        int ans = 0;
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
