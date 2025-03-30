2f66d3a2a8d2e44af0a6bf5333aadc1712fc9ab6
Hyper18 <windhyperspace@gmail.com>
2025-03-31 01:04:09 +0800

    二叉树

--- LeetCode/src/Tree/Q104_二叉树的最大深度.java
+++ LeetCode/src/Tree/Q104_二叉树的最大深度.java
@@ -1,18 +1,20 @@
 package Tree;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 /**
  * @author Hyper
- * @date 2022/03/01
+ * @date 2022/03/01，2025/03/30
  * <p>
  * 思路
  * 1. dfs
+ * (1) 向下dfs
+ * (2) 向上dfs
  * 2. bfs
  */
 public class Q104_二叉树的最大深度 {
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1_0(TreeNode root) {
         return dfs(root);
     }
 
@@ -20,31 +22,38 @@ public class Q104_二叉树的最大深度 {
         if (cur == null) {
             return 0;
         }
-        int left = dfs(cur.left);
-        int right = dfs(cur.right);
+        int l = dfs(cur.left), r = dfs(cur.right);
 
-        return Math.max(left, right) + 1;
+        return Math.max(l, r) + 1;
+    }
+
+    private int maxDepth1_1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = maxDepth1_1(root.left), r = maxDepth1_1(root.right);
+
+        return Math.max(l, r) + 1;
     }
 
     public int maxDepth2(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
         int ans = 0;
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int curSize = queue.size();
-            while (curSize > 0) {
-                TreeNode cur = queue.poll();
-                if (cur.left != null) {
-                    queue.offer(cur.left);
+        while (!q.isEmpty()) {
+            int n = q.size();
+            while (n > 0) {
+                TreeNode p = q.poll();
+                if (p.left != null) {
+                    q.offer(p.left);
                 }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
+                if (p.right != null) {
+                    q.offer(p.right);
                 }
-                curSize--;
+                n--;
             }
             ans++;
         }
