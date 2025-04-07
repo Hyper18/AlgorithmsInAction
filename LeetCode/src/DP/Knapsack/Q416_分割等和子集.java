a0860ec349cf379b05a72aa4aa5a23afaa200aba
Hyper18 <windhyperspace@gmail.com>
2025-04-07 10:07:10 +0800

    7th, Apr

--- LeetCode/src/DP/Knapsack/Q416_分割等和子集.java
+++ LeetCode/src/DP/Knapsack/Q416_分割等和子集.java
@@ -1,91 +1,102 @@
 package DP.Knapsack;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 /**
  * @author Hyper
- * @date 2022/03/19
- * 01背包问题
+ * @date 2022/03/19，2025/04/07
+ * 1. 01背包问题
  * 首先找dp含义
  * 对原模型
- * dp[i][j]表示
- * 外层循环表示从0 ~ i遍历物品
+ * f[i][j] 表示
+ * 外层循环表示从遍历物品
  * 内层循环表示从背包容量 ~ 能装入最小物品的容量
  * <p>
  * 而对于本问题，背包容量为数组所有元素和的一半
  * 结合“两个子集的元素和相等”的描述，为恰等于背包容量
  * <p>
- * 还可以直接把dp设为boolean数组，以压缩空间占用
- * 初始化dp[j = 0]
- * <p>
- * 注意i应对齐物品的下标
+ * 还可以直接把 f 设为布尔数组，以压缩空间占用
+ * 初始化 f[i = 0]
  * <p>
  * 还可以对时间复杂度做进一步优化：
- * 当数组中的最大数仍比half大（大于背包容量）时
- * 直接返回false
+ * 当数组中的最大数仍比一半大（大于背包容量）时
+ * 直接返回 false
+ * <p>
+ * 2. BitSet空间优化
  */
 public class Q416_分割等和子集 {
-    public boolean canPartition(int[] nums) {
-        int n = nums.length;
+    public boolean canPartition1_0(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         if (sum % 2 != 0) {
             return false;
         }
-        int half = sum / 2;
-        int[] dp = new int[half + 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = half; nums[i] <= j; j--) {
-                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+        int n = sum >> 1;
+        int[] f = new int[n + 1];
+        for (int num : nums) {
+            for (int i = n; i >= num; i--) {
+                f[i] = Math.max(f[i], f[i - num] + num);
             }
         }
-        if (dp[half] == half) {
-            return true;
+
+        return f[n] == n;
+    }
+
+    public boolean canPartition1_1(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int n = sum >> 1;
+        boolean[] f = new boolean[n + 1];
+        f[0] = true;
+        for (int num : nums) {
+            for (int i = n; i >= num; i--) {
+                f[i] = f[i] | f[i - num];
+            }
         }
 
-        return false;
+        return f[n];
+    }
+
+    public boolean canPartition1_2(int[] nums) {
+        int sum = 0, mx = Integer.MIN_VALUE;
+        for (int num : nums) {
+            sum += num;
+            mx = num > mx ? num : mx;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int n = sum / 2;
+        if (mx > n) {
+            return false;
+        }
+        boolean[] f = new boolean[n + 1];
+        f[0] = true;
+        for (int num : nums) {
+            for (int i = n; i >= num; i--) {
+                f[i] = f[i] | f[i - num];
+            }
+        }
+
+        return f[n];
     }
 
     public boolean canPartition2(int[] nums) {
-        int n = nums.length;
-        int sum = Arrays.stream(nums).sum();
-        if (sum % 2 != 0) {
-            return false;
-        }
-        int half = sum / 2;
-        boolean[] dp = new boolean[half + 1];
-        dp[0] = true;
-        for (int i = 0; i < n; i++) {
-            for (int j = half; nums[i] <= j; j--) {
-                dp[j] = dp[j] | dp[j - nums[i]];
-            }
-        }
-
-        return dp[half];
-    }
-
-    public boolean canPartition3(int[] nums) {
-        int n = nums.length;
         int sum = 0;
-        int max = -1;
         for (int num : nums) {
             sum += num;
-            max = num > max ? num : max;
         }
         if (sum % 2 != 0) {
             return false;
         }
-        int half = sum / 2;
-        if (max > half) {
-            return false;
-        }
-        boolean[] dp = new boolean[half + 1];
-        dp[0] = true;
-        for (int i = 0; i < n; i++) {
-            for (int j = half; nums[i] <= j; j--) {
-                dp[j] = dp[j] | dp[j - nums[i]];
-            }
+        sum >>= 1;
+        BigInteger f = BigInteger.ONE;
+        for (int num : nums) {
+            f = f.or(f.shiftLeft(num));
         }
 
-        return dp[half];
+        return f.testBit(sum);
     }
 }
