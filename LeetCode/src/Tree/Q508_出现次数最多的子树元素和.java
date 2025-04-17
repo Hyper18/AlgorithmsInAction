9303c7145a27cd336436ff1a9ff521b737fcbea7
Hyper18 <windhyperspace@gmail.com>
2025-04-17 23:13:04 +0800

    二叉树

--- LeetCode/src/Tree/Q508_出现次数最多的子树元素和.java
+++ LeetCode/src/Tree/Q508_出现次数最多的子树元素和.java
@@ -9,20 +9,19 @@ import java.util.Map;
  * @author Hyper
  * @date 2025/04/17
  * @file Q508_出现次数最多的子树元素和.java
+ * <p>
+ * 思路
+ * 向上dfs
  */
 public class Q508_出现次数最多的子树元素和 {
     private Map<Integer, Integer> mp = new HashMap<>();
+    private int mx;
 
     public int[] findFrequentTreeSum(TreeNode root) {
         dfs(root);
-        int mx = 0;
         List<Integer> res = new ArrayList<>();
         for (Map.Entry<Integer, Integer> e : mp.entrySet()) {
-            if (e.getValue() >= mx) {
-                if (e.getValue() > mx) {
-                    res.clear();
-                    mx = e.getValue();
-                }
+            if (e.getValue() == mx) {
                 res.add(e.getKey());
             }
         }
@@ -34,12 +33,9 @@ public class Q508_出现次数最多的子树元素和 {
         if (cur == null) {
             return 0;
         }
-        if (cur.left == null && cur.right == null) {
-            mp.merge(cur.val, 1, Integer::sum);
-            return cur.val;
-        }
         int sum = cur.val + dfs(cur.left) + dfs(cur.right);
         mp.merge(sum, 1, Integer::sum);
+        mx = Math.max(mx, mp.get(sum));
 
         return sum;
     }
