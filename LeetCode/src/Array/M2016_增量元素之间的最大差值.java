06fa7cb48f6e50bf92a52bb9b7606af8df0fc07c
Hyper18 <windhyperspace@gmail.com>
2025-06-16 21:26:13 +0800

    回溯

--- LeetCode/src/Array/M2016_增量元素之间的最大差值.java
+++ LeetCode/src/Array/M2016_增量元素之间的最大差值.java
@@ -2,7 +2,7 @@ package Array;
 
 /**
  * @author Hyper
- * @date 2022/02/26
+ * @date 2022/02/26，2025/06/16
  * <p>
  * 思路
  * 1. 暴力
@@ -25,17 +25,15 @@ public class M2016_增量元素之间的最大差值 {
     }
 
     public int maximumDifference2(int[] nums) {
-        int max = -1;
-        int n = nums.length;
-        int pre = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > pre) {
-                max = Math.max(max, nums[i] - pre);
+        int ans = -1, pre = nums[0];
+        for (int num : nums) {
+            if (num > pre) {
+                ans = Math.max(ans, num - pre);
             } else {
-                pre = nums[i];
+                pre = num;
             }
         }
 
-        return max;
+        return ans;
     }
 }
--- LeetCode/src/BackTracking/Q46_全排列.java
+++ LeetCode/src/BackTracking/Q46_全排列.java
@@ -6,37 +6,79 @@ import java.util.List;
 
 /**
  * @author Hyper
- * @date 2022/03/08
- * 回溯算法 -- O(n×n!)
- * 参考官解
+ * @date 2022/03/08，2025/06/16
+ * <p>
+ * 思路
+ * 排列型回溯 -- O(n×n!)
+ * 枚举选哪个
+ * <p>
+ * 注：对排列型回溯，选或不选的思路不适用
  */
 public class Q46_全排列 {
-    private List<List<Integer>> res;
-    private List<Integer> permutation;
-    int n;
+    class Solution1_0 {
+        private List<List<Integer>> res;
+        private List<Integer> permutation;
+        private int n;
 
-    public List<List<Integer>> permute(int[] nums) {
-        n = nums.length;
-        res = new ArrayList<>();
-        permutation = new ArrayList<>();
-        for (int num : nums) {
-            permutation.add(num);
+        public List<List<Integer>> permute(int[] nums) {
+            n = nums.length;
+            res = new ArrayList<>();
+            permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+
+            backtrack(0);
+            return res;
         }
 
-        backtrack(0);
-        return res;
+        private void backtrack(int idx) {
+            if (idx == n) {
+                res.add(new ArrayList<>(permutation));
+            }
+            for (int i = idx; i < n; i++) {
+                // 动态维护数组
+                Collections.swap(permutation, idx, i);
+                backtrack(idx + 1);
+                // 执行撤销操作，回溯
+                Collections.swap(permutation, idx, i);
+            }
+        }
     }
 
-    private void backtrack(int idx) {
-        if (idx == n) {
-            res.add(new ArrayList<>(permutation));
+    class Solution1_1 {
+        private List<List<Integer>> res = new ArrayList<>();
+        private List<Integer> path = new ArrayList<>();
+        private boolean[] vis;
+        private int[] nums;
+        private int n;
+
+        public List<List<Integer>> permute(int[] nums) {
+            n = nums.length;
+            this.nums = nums;
+            vis = new boolean[n];
+            for (int num : nums) {
+                path.add(num);
+            }
+            backtrack(0);
+
+            return res;
         }
-        for (int i = idx; i < n; i++) {
-            // 动态维护数组
-            Collections.swap(permutation, idx, i);
-            backtrack(idx + 1);
-            // 执行撤销操作，回溯
-            Collections.swap(permutation, idx, i);
+
+        private void backtrack(int i) {
+            if (i == n) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int j = 0; j < n; j++) {
+                if (vis[j]) {
+                    continue;
+                }
+                path.set(i, nums[j]);
+                vis[j] = true;
+                backtrack(i + 1);
+                vis[j] = false;
+            }
         }
     }
 }
